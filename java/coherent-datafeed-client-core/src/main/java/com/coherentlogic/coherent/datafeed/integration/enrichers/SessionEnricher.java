72265c4c5223714b87cba15eea9ea57cf2822f1d
thospfuller <thospfuller@bitbucket.org>
2015-11-16 09:09:47 -0500

    Spring integration upgrade.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/integration/enrichers/SessionEnricher.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/integration/enrichers/SessionEnricher.java
@@ -2,8 +2,8 @@ package com.coherentlogic.coherent.datafeed.integration.enrichers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.integration.Message;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 
 import com.coherentlogic.coherent.datafeed.services.Session;
 
