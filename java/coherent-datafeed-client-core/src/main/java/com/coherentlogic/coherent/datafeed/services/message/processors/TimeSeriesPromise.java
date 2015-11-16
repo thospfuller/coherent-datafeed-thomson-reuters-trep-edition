c7caa8348285383e0a6b589eb2f0ccf37cf64272
thospfuller <thospfuller@bitbucket.org>
2015-11-16 10:03:52 -0500

    New file added.

---
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TimeSeriesPromise.java
@@ -0,0 +1,27 @@
+package com.coherentlogic.coherent.datafeed.services.message.processors;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.coherentlogic.coherent.datafeed.domain.TimeSeries;
+import com.reuters.rfa.common.Handle;
+
+/**
+ * A promise that includes the TimeSeries as well as the handle which is associated with it.
+ *
+ * @see TransformTimeSeriesMessageProcessor (IMPORTANT)
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ */
+public class TimeSeriesPromise extends CompletableFuture<TimeSeries> {
+
+    private final Handle handle;
+
+    public TimeSeriesPromise(Handle handle) {
+        super();
+        this.handle = handle;
+    }
+
+    public Handle getHandle() {
+        return handle;
+    }
+}
