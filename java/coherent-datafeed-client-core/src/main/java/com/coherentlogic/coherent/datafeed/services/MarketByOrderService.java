7d0359a698022615caeb0ca6cec8cb5d2c5b238b
thospfuller <thospfuller@bitbucket.org>
2015-11-26 22:00:46 -0500

    The executeRequest method implementation details have been added.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketByOrderService.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketByOrderService.java
@@ -5,11 +5,16 @@ import java.util.List;
 import javax.jms.MessageConsumer;
 
 import com.coherentlogic.coherent.datafeed.adapters.BasicAdapter;
+import com.coherentlogic.coherent.datafeed.builders.RequestMessageBuilder;
 import com.coherentlogic.coherent.datafeed.domain.MarketByOrder;
+import com.coherentlogic.coherent.datafeed.domain.MarketPrice;
 import com.coherentlogic.coherent.datafeed.factories.RequestMessageBuilderFactory;
 import com.coherentlogic.coherent.datafeed.misc.Constants;
 import com.reuters.rfa.common.Client;
 import com.reuters.rfa.common.Handle;
+import com.reuters.rfa.omm.OMMMsg;
+import com.reuters.rfa.omm.OMMPriority;
+import com.reuters.rfa.rdm.RDMInstrument;
 import com.reuters.rfa.rdm.RDMMsgTypes;
 
 /**
@@ -20,7 +25,8 @@ import com.reuters.rfa.rdm.RDMMsgTypes;
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
 public class MarketByOrderService
-    extends AsynchronousService<MarketByOrder> {
+    extends AsynchronousService<MarketByOrder>
+    implements MarketByOrderServiceSpecification {
 
     public MarketByOrderService(
         RequestMessageBuilderFactory factory,
@@ -28,7 +34,6 @@ public class MarketByOrderService
         MessageConsumer messageConsumer,
         BasicAdapter<MarketByOrder, String> jsonGenerator
     ) {
-        // Should this be dIDN_RDF?
         super(
             Constants.dELEKTRON_DD,
             RDMMsgTypes.MARKET_BY_ORDER,
@@ -39,6 +44,10 @@ public class MarketByOrderService
         );
     }
 
+    /**
+     * @TODO: Copied verbatim from the MarketPriceService so we need to refactor
+     *  this.
+     */
     @Override
     protected List<Handle> executeRequest(
         String serviceName,
@@ -46,7 +55,23 @@ public class MarketByOrderService
         short msgModelType,
         String... itemNames
     ) {
-        throw new RuntimeException("Method not implemented.");
+    	RequestMessageBuilderFactory factory =
+            getRequestMessageBuilderFactory();
+        Client client = getClient();
+
+        RequestMessageBuilder builder = factory.getInstance();
+
+        List<Handle> handles = builder
+            .createOMMMsg()
+            .setMsgType(OMMMsg.MsgType.REQUEST)
+            .setMsgModelType(msgModelType)
+            .setIndicationFlags(OMMMsg.Indication.REFRESH)
+            .setAttribInfo(serviceName, RDMInstrument.NameType.RIC, itemNames)
+            .setPriority(OMMPriority.DEFAULT)
+            .setAssociatedMetaInfo(loginHandle)
+            .register(client, serviceName, itemNames);
+
+        return handles;
     }
 }
 
