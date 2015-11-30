8637f71f94e9273023bc8b71ad614ddf6fd0d50f
thospfuller <thospfuller@bitbucket.org>
2015-11-29 21:38:20 -0500

    New file added.

---
+++ java/coherent-datafeed-misc/src/main/java/com/coherentlogic/coherent/datafeed/exceptions/InvalidApplicationIdException.java
@@ -0,0 +1,20 @@
+package com.coherentlogic.coherent.datafeed.exceptions;
+
+/**
+ * An exception that is thrown when the application id is invalid (ie. null or
+ * an empty string).
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ */
+public class InvalidApplicationIdException extends FatalRuntimeException {
+
+    private static final long serialVersionUID = -7323571199844035282L;
+
+	public InvalidApplicationIdException(String msg) {
+        super(msg);
+    }
+
+    public InvalidApplicationIdException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
