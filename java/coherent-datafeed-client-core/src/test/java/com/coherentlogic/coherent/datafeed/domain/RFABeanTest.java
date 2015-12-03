867e1ad9c27f0c6740d37772b45b8ccd1af642fd
thospfuller <thospfuller@bitbucket.org>
2015-12-03 17:32:23 -0500

    New test added.

---
+++ java/coherent-datafeed-client-core/src/test/java/com/coherentlogic/coherent/datafeed/domain/RFABeanTest.java
@@ -0,0 +1,54 @@
+package com.coherentlogic.coherent.datafeed.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RFABeanTest {
+
+    private RFABean bean = null;
+
+    @Before
+    public void setUp() throws Exception {
+        bean = new StatusResponse();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        bean = null;
+    }
+
+    @Test
+    public void testNotDiffers1() {
+
+        boolean result = bean.differs(null, null);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testNotDiffes2() {
+
+        boolean result = bean.differs("abc", "abc");
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testDiffers1() {
+
+        boolean result = bean.differs(null, "");
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void testDiffers2() {
+
+        boolean result = bean.differs("abc", "123");
+
+        assertTrue(result);
+    }
+}
