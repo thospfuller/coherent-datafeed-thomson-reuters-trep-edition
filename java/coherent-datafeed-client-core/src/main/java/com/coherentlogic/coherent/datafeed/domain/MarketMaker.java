c9eaa2b4eab1561b06d6ed618d6833282f78e212
thospfuller <thomas.fuller@coherentlogic.com>
2016-03-25 17:05:13 -0400

    Implementation details added.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/MarketMaker.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/MarketMaker.java
@@ -3,199 +3,407 @@ package com.coherentlogic.coherent.datafeed.domain;
 import static com.coherentlogic.coherent.datafeed.misc.Constants.MARKET_MAKER;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.Entity;
 
+import com.coherentlogic.coherent.datafeed.adapters.omm.OMMDataBufferAdapter;
 import com.coherentlogic.coherent.datafeed.adapters.omm.OMMDateTimeAdapter;
 import com.coherentlogic.coherent.datafeed.adapters.omm.OMMEnumAdapter;
 import com.coherentlogic.coherent.datafeed.adapters.omm.OMMNumericAdapter;
 import com.coherentlogic.coherent.datafeed.annotations.Adapt;
 import com.coherentlogic.coherent.datafeed.annotations.RFAType;
+import com.coherentlogic.coherent.datafeed.annotations.UsingKey;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 @Entity
 @XStreamAlias(MARKET_MAKER)
-public class MarketMaker extends RFABean implements MarketPriceConstants {
+public class MarketMaker extends AbstractAdvancedCommonProperties
+    implements MarketPriceConstants {
 
-    private static final long serialVersionUID = 1L;
+    @XStreamAlias(OFFCL_CODE)
+    private String officialCode;
 
-    @XStreamAlias(BID)
-    private BigDecimal bid = null;
+    /**
+     * NASDSTATUS / enum
+     */
+    @XStreamAlias(NASDSTATUS)
+    private String nasdStatus;
 
-    @XStreamAlias(ASK)
-    private BigDecimal ask = null;
+    /**
+     * REAL64
+     */
+    @XStreamAlias(LOT_SIZE_A)
+    private BigDecimal lotSize;
 
-    @XStreamAlias(BIDSIZE)
-    private BigDecimal bidSize = null;
+    /**
+     * Enum
+     */
+    @XStreamAlias(OFF_CD_IND)
+    private String officialCodeIndicator;
 
-    @XStreamAlias(ASKSIZE)
-    private BigDecimal askSize = null;
+    /**
+     * RMTES_STRING
+     */
+    @XStreamAlias(LIST_MKT)
+    private String listingMarket = null;
 
-    @XStreamAlias(MKT_MKR_NM)
-    private String marketMakerName = null;
+    /**
+     * RMTES_STRING
+     */
+    @XStreamAlias(CLASS_CODE)
+    private String instrumentClass = null;
 
-    @XStreamAlias(MMID)
-    private String marketMakerId = null;
+    /**
+     * RMTES_STRING
+     */
+    @XStreamAlias(PERIOD_CDE)
+    private String periodCode = null;
 
-    @XStreamAlias(ASK_TIM_MS)
-    private Long askTimeMillis = null;
+    @XStreamAlias(FIN_STATUS)
+    private String financialStatusIndicator = null;
 
-    @XStreamAlias(TIMACT_MS)
-    private Long lastActivityTimeMillis = null;
+    @XStreamAlias(MKT_STATUS)
+    private String marketStatusIndicator = null;
 
-    @XStreamAlias(BID_TIM_MS)
-    private Long bidTimeMillis = null;
+    @XStreamAlias(ORDERS)
+    private final Map<String, Order> orders;
 
-    @XStreamAlias(PRIMARY_MM)
-    private Boolean primaryMarketMaker = null;
-
-    @XStreamAlias(MM_MODE)
-    private Boolean marketMakerMode = null;
-
-    @XStreamAlias(MM_STATE)
-    private Boolean marketMakerState = null;
-
-    @XStreamAlias(PR_TIM_MS)
-    private Long priorityTimeMillis = null;
-
-    @XStreamAlias(PR_DATE)
-    private Long priortyDate = null;
-
-    public BigDecimal getBid() {
-        return bid;
+    public MarketMaker () {
+        this (new HashMap<String, Order> ());
     }
 
-    @RFAType(type=BID)
-    @Adapt(using=OMMNumericAdapter.class)
-    public void setBid(BigDecimal bid) {
-        this.bid = bid;
+    public MarketMaker (Map<String, Order> orders) {
+        this.orders = orders;
     }
 
-    public BigDecimal getAsk() {
-        return ask;
+    public String getOfficialCode() {
+        return officialCode;
     }
 
-    @RFAType(type=ASK)
-    @Adapt(using=OMMNumericAdapter.class)
-    public void setAsk(BigDecimal ask) {
-        this.ask = ask;
+
+    public void setOfficialCode(String officialCode) {
+        this.officialCode = officialCode;
     }
 
-    public BigDecimal getBidSize() {
-        return bidSize;
+    
+    public String getNasdStatus() {
+        return nasdStatus;
     }
 
-    @RFAType(type=BIDSIZE)
-    @Adapt(using=OMMNumericAdapter.class)
-    public void setBidSize(BigDecimal bidSize) {
-        this.bidSize = bidSize;
+
+    public void setNasdStatus(String nasdStatus) {
+        this.nasdStatus = nasdStatus;
     }
 
-    public BigDecimal getAskSize() {
-        return askSize;
+
+    public BigDecimal getLotSize() {
+        return lotSize;
     }
 
-    @RFAType(type=ASKSIZE)
-    @Adapt(using=OMMNumericAdapter.class)
-    public void setAskSize(BigDecimal askSize) {
-        this.askSize = askSize;
+
+    public void setLotSize(BigDecimal lotSize) {
+        this.lotSize = lotSize;
     }
 
-    public String getMarketMakerName() {
-        return marketMakerName;
+
+    public String getOfficialCodeIndicator() {
+        return officialCodeIndicator;
     }
 
-    @RFAType(type=MKT_MKR_NM)
-    public void setMarketMakerName(String marketMakerName) {
-        this.marketMakerName = marketMakerName;
+
+    public void setOfficialCodeIndicator(String officialCodeIndicator) {
+        this.officialCodeIndicator = officialCodeIndicator;
     }
 
-    public String getMarketMakerId() {
-        return marketMakerId;
+
+    public String getListingMarket() {
+        return listingMarket;
     }
 
-    @RFAType(type=MMID)
-    public void setMarketMakerId(String marketMakerId) {
-        this.marketMakerId = marketMakerId;
+
+    public void setListingMarket(String listingMarket) {
+        this.listingMarket = listingMarket;
     }
 
-    public Long getAskTimeMillis() {
-        return askTimeMillis;
+
+    public String getInstrumentClass() {
+        return instrumentClass;
     }
 
-    @RFAType(type=ASK_TIM_MS)
-    @Adapt(using=OMMNumericAdapter.class)
-    public void setAskTimeMillis(Long askTimeMillis) {
-        this.askTimeMillis = askTimeMillis;
+
+    public void setInstrumentClass(String instrumentClass) {
+        this.instrumentClass = instrumentClass;
     }
 
-    public Long getLastActivityTimeMillis() {
-        return lastActivityTimeMillis;
+
+    public String getPeriodCode() {
+        return periodCode;
     }
 
-    @RFAType(type=TIMACT_MS)
-    @Adapt(using=OMMDateTimeAdapter.class)
-    public void setLastActivityTimeMillis(Long lastActivityTimeMillis) {
-        this.lastActivityTimeMillis = lastActivityTimeMillis;
+
+    public void setPeriodCode(String periodCode) {
+        this.periodCode = periodCode;
     }
 
-    public Long getBidTimeMillis() {
-        return bidTimeMillis;
+
+    public String getFinancialStatusIndicator() {
+        return financialStatusIndicator;
     }
 
-    @RFAType(type=BID_TIM_MS)
-    @Adapt(using=OMMDateTimeAdapter.class)
-    public void setBidTimeMillis(Long bidTimeMillis) {
-        this.bidTimeMillis = bidTimeMillis;
+
+    public void setFinancialStatusIndicator(String financialStatusIndicator) {
+        this.financialStatusIndicator = financialStatusIndicator;
     }
 
-    public Boolean getPrimaryMarketMaker() {
-        return primaryMarketMaker;
+
+    public String getMarketStatusIndicator() {
+        return marketStatusIndicator;
     }
 
-    @RFAType(type=PRIMARY_MM)
-    @Adapt(using=OMMEnumAdapter.class)
-    public void setPrimaryMarketMaker(Boolean primaryMarketMaker) {
-        this.primaryMarketMaker = primaryMarketMaker;
+
+    public void setMarketStatusIndicator(String marketStatusIndicator) {
+        this.marketStatusIndicator = marketStatusIndicator;
     }
 
-    public Boolean getMarketMakerMode() {
-        return marketMakerMode;
+    public Map<String, Order> getOrders() {
+        return orders;
     }
 
-    @RFAType(type=MM_MODE)
-    @Adapt(using=OMMEnumAdapter.class)
-    public void setMarketMakerMode(Boolean marketMakerMode) {
-        this.marketMakerMode = marketMakerMode;
+    @Override
+    public String toString() {
+        return "MarketMaker [officialCode=" + officialCode + ", nasdStatus="
+            + nasdStatus + ", lotSize=" + lotSize
+            + ", officialCodeIndicator=" + officialCodeIndicator
+            + ", listingMarket=" + listingMarket + ", instrumentClass="
+            + instrumentClass + ", periodCode=" + periodCode
+            + ", financialStatusIndicator=" + financialStatusIndicator
+            + ", marketStatusIndicator=" + marketStatusIndicator
+            + ", orders=" + orders + "]";
     }
 
-    public Boolean getMarketMakerState() {
-        return marketMakerState;
-    }
+    /**
+     * 
+     *
+     * @author <a href="mailto:support@coherentlogic.com">Support</a>
+     */
+    public static class Order extends RFABean {
 
-    @RFAType(type=MM_STATE)
-    @Adapt(using=OMMEnumAdapter.class)
-    public void setMarketMakerState(Boolean marketMakerState) {
-        this.marketMakerState = marketMakerState;
-    }
+        @XStreamAlias(BID)
+        private BigDecimal bid = null;
 
-    public Long getPriorityTimeMillis() {
-        return priorityTimeMillis;
-    }
+        @XStreamAlias(ASK)
+        private BigDecimal ask = null;
 
-    @RFAType(type=PR_TIM_MS)
-    @Adapt(using=OMMDateTimeAdapter.class)
-    public void setPriorityTimeMillis(Long priorityTimeMillis) {
-        this.priorityTimeMillis = priorityTimeMillis;
-    }
+        @XStreamAlias(BIDSIZE)
+        private BigDecimal bidSize = null;
 
-    public Long getPriortyDate() {
-        return priortyDate;
-    }
+        @XStreamAlias(ASKSIZE)
+        private BigDecimal askSize = null;
 
-    @RFAType(type=PR_DATE)
-    @Adapt(using=OMMDateTimeAdapter.class)
-    public void setPriortyDate(Long priortyDate) {
-        this.priortyDate = priortyDate;
+        @XStreamAlias(MKT_MKR_NM)
+        private String marketMakerName = null;
+
+        @XStreamAlias(MMID)
+        private String marketMakerId = null;
+
+        @XStreamAlias(ASK_TIM_MS)
+        private Long askTimeMillis = null;
+
+        @XStreamAlias(TIMACT_MS)
+        private Long lastActivityTimeMillis = null;
+
+        @XStreamAlias(BID_TIM_MS)
+        private Long bidTimeMillis = null;
+
+        @XStreamAlias(PRIMARY_MM)
+        private Boolean primaryMarketMaker = null;
+
+        @XStreamAlias(MM_MODE)
+        private Boolean marketMakerMode = null;
+
+        @XStreamAlias(MM_STATE)
+        private Boolean marketMakerState = null;
+
+        @XStreamAlias(PR_TIM_MS)
+        private Long priorityTimeMillis = null;
+
+        @XStreamAlias(PR_DATE)
+        private Long priortyDate = null;
+
+        @UsingKey(type=BID)
+        public BigDecimal getBid() {
+            return bid;
+        }
+
+        @RFAType(type=BID)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setBid(BigDecimal bid) {
+            this.bid = bid;
+        }
+
+        @UsingKey(type=ASK)
+        public BigDecimal getAsk() {
+            return ask;
+        }
+
+        @RFAType(type=ASK)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setAsk(BigDecimal ask) {
+            this.ask = ask;
+        }
+
+        @UsingKey(type=BIDSIZE)
+        public BigDecimal getBidSize() {
+            return bidSize;
+        }
+
+        @RFAType(type=BIDSIZE)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setBidSize(BigDecimal bidSize) {
+            this.bidSize = bidSize;
+        }
+
+        @UsingKey(type=ASKSIZE)
+        public BigDecimal getAskSize() {
+            return askSize;
+        }
+
+        /**
+         * @TODO: SHOULD THIS BE AN INTEGER OR LONG?
+         *
+         * @param askSize
+         */
+        @RFAType(type=ASKSIZE)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setAskSize(BigDecimal askSize) {
+            this.askSize = askSize;
+        }
+
+        @UsingKey(type=MKT_MKR_NM)
+        public String getMarketMakerName() {
+            return marketMakerName;
+        }
+
+        @RFAType(type=MKT_MKR_NM)
+        @Adapt(using=OMMDataBufferAdapter.class)
+        public void setMarketMakerName(String marketMakerName) {
+            this.marketMakerName = marketMakerName;
+        }
+
+        @UsingKey(type=MMID)
+        public String getMarketMakerId() {
+            return marketMakerId;
+        }
+
+        @RFAType(type=MMID)
+        @Adapt(using=OMMDataBufferAdapter.class)
+        public void setMarketMakerId(String marketMakerId) {
+            this.marketMakerId = marketMakerId;
+        }
+
+        @UsingKey(type=ASK_TIM_MS)
+        public Long getAskTimeMillis() {
+            return askTimeMillis;
+        }
+
+        @RFAType(type=ASK_TIM_MS)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setAskTimeMillis(Long askTimeMillis) {
+            this.askTimeMillis = askTimeMillis;
+        }
+
+        @UsingKey(type=TIMACT_MS)
+        public Long getLastActivityTimeMillis() {
+            return lastActivityTimeMillis;
+        }
+
+        @RFAType(type=TIMACT_MS)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setLastActivityTimeMillis(Long lastActivityTimeMillis) {
+            this.lastActivityTimeMillis = lastActivityTimeMillis;
+        }
+
+        @UsingKey(type=BID_TIM_MS)
+        public Long getBidTimeMillis() {
+            return bidTimeMillis;
+        }
+
+        @RFAType(type=BID_TIM_MS)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setBidTimeMillis(Long bidTimeMillis) {
+            this.bidTimeMillis = bidTimeMillis;
+        }
+
+        @UsingKey(type=PRIMARY_MM)
+        public Boolean getPrimaryMarketMaker() {
+            return primaryMarketMaker;
+        }
+
+        @RFAType(type=PRIMARY_MM)
+        @Adapt(using=OMMEnumAdapter.class)
+        public void setPrimaryMarketMaker(Boolean primaryMarketMaker) {
+            this.primaryMarketMaker = primaryMarketMaker;
+        }
+
+        @UsingKey(type=MM_MODE)
+        public Boolean getMarketMakerMode() {
+            return marketMakerMode;
+        }
+
+        @RFAType(type=MM_MODE)
+        @Adapt(using=OMMEnumAdapter.class)
+        public void setMarketMakerMode(Boolean marketMakerMode) {
+            this.marketMakerMode = marketMakerMode;
+        }
+
+        @UsingKey(type=MM_STATE)
+        public Boolean getMarketMakerState() {
+            return marketMakerState;
+        }
+
+        @RFAType(type=MM_STATE)
+        @Adapt(using=OMMEnumAdapter.class)
+        public void setMarketMakerState(Boolean marketMakerState) {
+            this.marketMakerState = marketMakerState;
+        }
+
+        @UsingKey(type=PR_TIM_MS)
+        public Long getPriorityTimeMillis() {
+            return priorityTimeMillis;
+        }
+
+        @RFAType(type=PR_TIM_MS)
+        @Adapt(using=OMMNumericAdapter.class)
+        public void setPriorityTimeMillis(Long priorityTimeMillis) {
+            this.priorityTimeMillis = priorityTimeMillis;
+        }
+
+        @UsingKey(type=PR_DATE)
+        public Long getPriortyDate() {
+            return priortyDate;
+        }
+
+        @RFAType(type=PR_DATE)
+        @Adapt(using=OMMDateTimeAdapter.class)
+        public void setPriortyDate(Long priortyDate) {
+            this.priortyDate = priortyDate;
+        }
+
+        @Override
+        public String toString() {
+            return "Order [bid=" + bid + ", ask=" + ask + ", bidSize="
+                + bidSize + ", askSize=" + askSize + ", marketMakerName="
+                + marketMakerName + ", marketMakerId=" + marketMakerId
+                + ", askTimeMillis=" + askTimeMillis
+                + ", lastActivityTimeMillis=" + lastActivityTimeMillis
+                + ", bidTimeMillis=" + bidTimeMillis
+                + ", primaryMarketMaker=" + primaryMarketMaker
+                + ", marketMakerMode=" + marketMakerMode
+                + ", marketMakerState=" + marketMakerState
+                + ", priorityTimeMillis=" + priorityTimeMillis
+                + ", priortyDate=" + priortyDate + "]";
+        }
     }
 }
