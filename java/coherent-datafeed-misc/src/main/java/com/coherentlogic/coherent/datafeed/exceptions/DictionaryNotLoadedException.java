7632bc88ae0d3199ad60be2308c629a4d5bbe716
thospfuller <thomas.fuller@coherentlogic.com>
2016-04-26 12:22:35 -0400

    New exception added.

---
+++ java/coherent-datafeed-misc/src/main/java/com/coherentlogic/coherent/datafeed/exceptions/DictionaryNotLoadedException.java
@@ -0,0 +1,18 @@
+package com.coherentlogic.coherent.datafeed.exceptions;
+
+/**
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ */
+public class DictionaryNotLoadedException extends FatalRuntimeException {
+
+	private static final long serialVersionUID = 2479729850575982941L;
+
+	public DictionaryNotLoadedException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public DictionaryNotLoadedException(String msg) {
+		super(msg);
+	}
+}
