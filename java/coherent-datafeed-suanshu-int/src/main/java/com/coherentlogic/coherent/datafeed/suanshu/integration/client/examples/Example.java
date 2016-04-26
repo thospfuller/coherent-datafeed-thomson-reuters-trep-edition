42895fe29c0525f523ea51297c84d6dfcb8f1d91
thospfuller <thomas.fuller@coherentlogic.com>
2016-04-26 12:23:18 -0400

    New class added.

---
+++ java/coherent-datafeed-suanshu-int/src/main/java/com/coherentlogic/coherent/datafeed/suanshu/integration/client/examples/Example.java
@@ -0,0 +1,12 @@
+package com.coherentlogic.coherent.datafeed.suanshu.integration.client.examples;
+
+/**
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ */
+public class Example {
+
+	public static void main(String[] args) {
+		
+	}
+}
