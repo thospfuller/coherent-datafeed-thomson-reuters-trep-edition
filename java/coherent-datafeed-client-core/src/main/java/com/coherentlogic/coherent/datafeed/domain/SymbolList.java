00d8b13cbad389064e3924a247ae4ab147906c5d
thospfuller <thomas.fuller@coherentlogic.com>
2016-04-26 12:24:28 -0400

    New file added.

---
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/SymbolList.java
@@ -0,0 +1,13 @@
+package com.coherentlogic.coherent.datafeed.domain;
+
+/**
+ * 0#BMD
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ *
+ */
+public class SymbolList {
+
+    public SymbolList() {
+    }
+}
