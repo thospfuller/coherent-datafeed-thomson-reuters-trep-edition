6b3e2f32ce7d5de321db196c8e00ca3acc4c90df
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-01 21:40:28 -0400

    Class now extends from CacheableQueryableService and several properties have been added.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketMakerService.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketMakerService.java
@@ -1,8 +1,12 @@
 package com.coherentlogic.coherent.datafeed.services;
 
 import java.util.List;
+import java.util.Map;
 
+import com.coherentlogic.coherent.data.model.core.factories.TypedFactory;
 import com.coherentlogic.coherent.datafeed.builders.RequestMessageBuilder;
+import com.coherentlogic.coherent.datafeed.domain.MarketByOrder;
+import com.coherentlogic.coherent.datafeed.domain.MarketMaker;
 import com.coherentlogic.coherent.datafeed.factories.RequestMessageBuilderFactory;
 import com.coherentlogic.coherent.datafeed.misc.Constants;
 import com.reuters.rfa.common.Client;
@@ -20,24 +24,37 @@ import com.reuters.rfa.rdm.RDMMsgTypes;
  * @author <a href="support@coherentlogic.com">Support</a>
  */
 public class MarketMakerService
-    extends QueryableService
+    extends CacheableQueryableService<MarketMaker>
     implements MarketMakerServiceSpecification {
 
-	public static final String BEAN_NAME = "marketMakerService";
+    public static final String BEAN_NAME = "marketMakerService";
 
-	/**
-	 * @deprecated The jsonGenerator should be removed from this class.
-	 */
+//    private final Map<Handle, String> ricCache;
+//
+//    private final Map<String, MarketMaker> marketMakerCache;
+
+    /**
+     * @deprecated The jsonGenerator should be removed from this class.
+     */
     public MarketMakerService(
         RequestMessageBuilderFactory factory,
-        Client client
+        Client client,
+        Map<Handle, String> ricCache,
+        Map<String, MarketMaker> marketMakerCache,
+        TypedFactory<MarketMaker> marketMakerFactory
     ) {
         super(
             Constants.dELEKTRON_DD,
             RDMMsgTypes.MARKET_MAKER,
             factory,
-            client
+            client,
+            ricCache,
+            marketMakerCache,
+            marketMakerFactory
         );
+
+//        this.ricCache = ricCache;
+//        this.marketMakerCache = marketMakerCache;
     }
 
     /**
@@ -69,4 +86,9 @@ public class MarketMakerService
 
         return handles;
     }
+
+//    @Override
+//    public Map<String, MarketMaker> query(ServiceName serviceName, Handle loginHandle, String... rics) {
+//
+//    }
 }
--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketPriceService.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketPriceService.java
@@ -1,20 +1,17 @@
 package com.coherentlogic.coherent.datafeed.services;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.coherentlogic.coherent.data.model.core.factories.TypedFactory;
 import com.coherentlogic.coherent.datafeed.builders.RequestMessageBuilder;
 import com.coherentlogic.coherent.datafeed.domain.MarketPrice;
-import com.coherentlogic.coherent.datafeed.exceptions.InvalidStateException;
 import com.coherentlogic.coherent.datafeed.factories.RequestMessageBuilderFactory;
 import com.coherentlogic.coherent.datafeed.misc.Constants;
-import com.coherentlogic.coherent.datafeed.misc.Utils;
 import com.reuters.rfa.common.Client;
 import com.reuters.rfa.common.Handle;
 import com.reuters.rfa.omm.OMMMsg;
@@ -33,31 +30,35 @@ import com.reuters.rfa.rdm.RDMMsgTypes;
  * @author <a href="support@coherentlogic.com">Support</a>
  */
 public class MarketPriceService
-    extends QueryableService
+    extends CacheableQueryableService<MarketPrice>
     implements MarketPriceServiceSpecification {
 
     private static final Logger log =
         LoggerFactory.getLogger(MarketPriceService.class);
 
-    private final Map<Handle, String> ricCache;
-
-    private final Map<String, MarketPrice> marketPriceCache;
+//    private final Map<Handle, String> ricCache;
+//
+//    private final Map<String, MarketPrice> marketPriceCache;
 
     public MarketPriceService(
         RequestMessageBuilderFactory factory,
         Client client,
         Map<Handle, String> ricCache,
-        Map<String, MarketPrice> marketPriceCache
+        Map<String, MarketPrice> marketPriceCache,
+        TypedFactory<MarketPrice> marketPriceFactory
     ) {
         super(
             Constants.dELEKTRON_DD,
             RDMMsgTypes.MARKET_PRICE,
             factory,
-            client
+            client,
+            ricCache,
+            marketPriceCache,
+            marketPriceFactory
         );
 
-        this.ricCache = ricCache;
-        this.marketPriceCache = marketPriceCache;
+//        this.ricCache = ricCache;
+//        this.marketPriceCache = marketPriceCache;
     }
 
     /**
@@ -121,54 +122,54 @@ public class MarketPriceService
         return query (serviceName, loginHandle, new String[] {item});
     }
 
-    @Override
-    public Map<String, MarketPrice> query(ServiceName serviceName, Handle loginHandle, String... rics) {
-
-        Utils.assertNotNull("serviceName", serviceName);
-        Utils.assertNotNull("rics", rics);
-
-        Map<String, MarketPrice> result = new HashMap<String, MarketPrice> (rics.length);
-
-        for (String ric : rics) {
-
-            MarketPrice marketPrice = marketPriceCache.get(ric);
-
-            Handle handle = findHandle (ric);
-
-            if (marketPrice != null && handle == null)
-                throw new InvalidStateException ("Invalid state detected: the marketPrice is not null however there " +
-                    "is no handle associated with the ric " + ric);
-            else if (marketPrice == null && handle != null)
-                throw new InvalidStateException ("Invalid state detected: the marketPrice is null however there is " +
-                    "a non-null handle associated with the ric " + ric);
-
-            if (marketPrice == null) {
-
-                marketPrice = new MarketPrice ();
-
-                marketPriceCache.put(ric, marketPrice);
-
-                List<Handle> handleList = query(serviceName.toString(), loginHandle, ric);
-
-                ricCache.put(handleList.get(0), ric);
-            }
-            result.put(ric, marketPrice);
-        }
-
-        return result;
-    }
-
-    Handle findHandle (String ric) {
-
-        Handle result = null;
-
-        for (Entry<Handle, String> nextEntry : ricCache.entrySet()) {
-            if (nextEntry.getValue().equals(ric)) {
-                result = nextEntry.getKey();
-                break;
-            }
-        }
-
-        return result;
-    }
+//    @Override
+//    public Map<String, MarketPrice> query(ServiceName serviceName, Handle loginHandle, String... rics) {
+//
+//        Utils.assertNotNull("serviceName", serviceName);
+//        Utils.assertNotNull("rics", rics);
+//
+//        Map<String, MarketPrice> result = new HashMap<String, MarketPrice> (rics.length);
+//
+//        for (String ric : rics) {
+//
+//            MarketPrice marketPrice = marketPriceCache.get(ric);
+//
+//            Handle handle = findHandle (ric);
+//
+//            if (marketPrice != null && handle == null)
+//                throw new InvalidStateException ("Invalid state detected: the marketPrice is not null however there " +
+//                    "is no handle associated with the ric " + ric);
+//            else if (marketPrice == null && handle != null)
+//                throw new InvalidStateException ("Invalid state detected: the marketPrice is null however there is " +
+//                    "a non-null handle associated with the ric " + ric);
+//
+//            if (marketPrice == null) {
+//
+//                marketPrice = new MarketPrice ();
+//
+//                marketPriceCache.put(ric, marketPrice);
+//
+//                List<Handle> handleList = query(serviceName.toString(), loginHandle, ric);
+//
+//                ricCache.put(handleList.get(0), ric);
+//            }
+//            result.put(ric, marketPrice);
+//        }
+//
+//        return result;
+//    }
+//
+//    Handle findHandle (String ric) {
+//
+//        Handle result = null;
+//
+//        for (Entry<Handle, String> nextEntry : ricCache.entrySet()) {
+//            if (nextEntry.getValue().equals(ric)) {
+//                result = nextEntry.getKey();
+//                break;
+//            }
+//        }
+//
+//        return result;
+//    }
 }
