ad4b1c4997187ecb6482cb24217a61c08182952c
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-11 15:43:03 -0400

    Added comments.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/CacheableQueryableService.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/CacheableQueryableService.java
@@ -48,6 +48,24 @@ public abstract class CacheableQueryableService<T> extends QueryableService {
 
             Handle handle = findHandle (ric);
 
+            /*
+             * Consider a proper association from handle to object looks like this:
+             *
+             * handle -> ric -> object
+             *
+             * So for below, if the object (marketPrice, for example) is not null but the handle is null then we have an
+             * invalid state because an object should always be associated with a handle and instead we have:
+             * 
+             * null -> ric -> object
+             *
+             * Likewise if the object is null and the handle is not then this is also invalid because the link from the
+             * handle to the object is broken:
+             *
+             * handle -> ric -> null
+             *
+             * NOTE: This scenario can happen when there's a cache shared between instances of this class (say MBO and
+             *       MP services. Caches should *not* be shared between services.
+             */
             if (object != null && handle == null)
                 throw new InvalidStateException ("Invalid state detected: the domain object is not null however " +
                     "there is no handle associated with the ric " + ric);
