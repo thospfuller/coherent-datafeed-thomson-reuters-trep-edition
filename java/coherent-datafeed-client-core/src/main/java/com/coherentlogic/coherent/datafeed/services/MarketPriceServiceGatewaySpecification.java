2dca5720785d7f07c858172e327f0e8c00f0cd6e
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-11 16:02:32 -0400

    New interfaces added.

---
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketPriceServiceGatewaySpecification.java
@@ -0,0 +1,21 @@
+package com.coherentlogic.coherent.datafeed.services;
+
+import java.util.Map;
+
+import com.coherentlogic.coherent.datafeed.domain.MarketPrice;
+import com.reuters.rfa.common.Handle;
+
+/**
+ * Used as a gateway interface into the Spring Integration workflow. Note that we have a very similar interface named
+ * ie. MarketMakerServiceSpecification however if we use this interface the framework won't work and the reason may have
+ * to do with AOP but it's not clear -- at one point we were seeing multiple method calls being executed when only one
+ * was expected. Adding another interface is not ideal but the issue oes not appear if we use this interface when
+ * calling into the integration workflow.
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ *
+ */
+public interface MarketPriceServiceGatewaySpecification {
+
+    Map<String, MarketPrice> query (ServiceName serviceName, Handle loginHandle, String... rics);
+}
