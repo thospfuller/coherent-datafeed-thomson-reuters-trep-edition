6326bbfa7c639b91b3ec6fb86e22424a96796360
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-24 12:27:31 -0400

    New class added.

---
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/StatusResponseBean.java
@@ -0,0 +1,28 @@
+package com.coherentlogic.coherent.datafeed.domain;
+
+import com.coherentlogic.coherent.datafeed.annotations.Changeable;
+
+/**
+ * A base class for beans which have a status response.
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ */
+public class StatusResponseBean extends RFABean {
+
+    private StatusResponse statusResponse;
+
+    public StatusResponse getStatusResponse() {
+        return statusResponse;
+    }
+
+    public static final String STATUS_RESPONSE = "statusResponse";
+
+    public void setStatusResponse(@Changeable(STATUS_RESPONSE) StatusResponse statusResponse) {
+        this.statusResponse = statusResponse;
+    }
+
+    @Override
+    public String toString() {
+        return "StatusResponseBean [statusResponse=" + statusResponse + ", toString()=" + super.toString() + "]";
+    }
+}
