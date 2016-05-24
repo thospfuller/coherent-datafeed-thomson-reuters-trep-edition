d99406a61fe528d2facf99a4527d7fac7289e7db
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-24 12:44:26 -0400

    Removed commented code.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/factories/MarketPriceFactory.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/factories/MarketPriceFactory.java
@@ -20,6 +20,5 @@ public class MarketPriceFactory implements TypedFactory<MarketPrice> {
     @Override
     public MarketPrice getInstance() {
         return applicationContext.getBean(MarketPrice.class);
-            // new MarketPrice ();
     }
 }
