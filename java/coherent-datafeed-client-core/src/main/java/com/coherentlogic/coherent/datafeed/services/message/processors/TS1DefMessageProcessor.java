d81f31039a57b361e16e2aad58fb6ba5077ed9bb
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-24 13:00:13 -0400

    Removed the transactional annotation, reformatted code, removed commented code, and changed the log level from info to debug.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TS1DefMessageProcessor.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TS1DefMessageProcessor.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.coherentlogic.coherent.datafeed.beans.TS1DefEntry;
 import com.coherentlogic.coherent.datafeed.services.MessageProcessorSpecification;
@@ -65,87 +64,33 @@ public class TS1DefMessageProcessor
      * As COMPLETION_EVENTS are received these will be removed from the 
      */
     @Override
-    @Transactional
-    public Message<OMMItemEvent> process(
-        Message<OMMItemEvent> message) {
+//    @Transactional
+    public Message<OMMItemEvent> process(Message<OMMItemEvent> message) {
 
-        log.info("ts1DefMessageProcessor.process: method begins; message: " +
-            message);
+        log.debug("ts1DefMessageProcessor.process: method begins; message: " + message);
 
-//        synchronized (ts1DefCache) {
+        MessageHeaders headers = message.getHeaders();
 
-            MessageHeaders headers = message.getHeaders();
+        Session session = (Session) headers.get(SESSION);
 
-            Session session = (Session) headers.get(SESSION);
+        assertNotNull(session);
 
-            assertNotNull(session);
+        Handle loginHandle = session.getLoginHandle();
 
-            Handle loginHandle = session.getLoginHandle();
+        List<Handle> handles = ts1DefService.initialize(loginHandle);
 
-            List<Handle> handles = ts1DefService.initialize(loginHandle);
+        log.debug("handles.size: " + handles.size());
 
-            log.info("handles.size: " + handles.size());
+        for (Handle nextHandle : handles) {
+            ts1DefCache.put (nextHandle, session);
+            TS1DefEntry ts1DefEntry = new TS1DefEntry ();
+            session.putTS1DefEntry(nextHandle, ts1DefEntry);
+            sessionCache.put(nextHandle, session);
+        }
 
-            for (Handle nextHandle : handles) {
-                ts1DefCache.put (nextHandle, session);
-                TS1DefEntry ts1DefEntry = new TS1DefEntry ();
-                session.putTS1DefEntry(nextHandle, ts1DefEntry);
-                sessionCache.put(nextHandle, session);
-            }
-//        }
-
-        log.info("ts1DefMessageProcessor.process: method ends; message: " +
+        log.debug("ts1DefMessageProcessor.process: method ends; message: " +
             ToStringBuilder.reflectionToString(message));
 
         return message;
     }
 }
-/*
-    @Override
-    public Message<List<Handle>> process(
-        Message<TS1DefQueryParameters> message) {
-
-        TS1DefQueryParameters ts1DefQueryParameters = message.getPayload();
-
-        Handle loginHandle = ts1DefQueryParameters.getLoginHandle();
-
-        MessageHeaders headers = null;
-
-        Message<List<Handle>> result = null;
-
-        synchronized (sessionCache) {
-
-            Session session = sessionCache.get(loginHandle);
-
-            String[] rics = ts1DefQueryParameters.getRics();
-
-            log.info("ts1DefQueryParameters: " + ts1DefQueryParameters);
-
-            List<Handle> handles = null;
-
-            if (rics == null || rics.length == 0)
-                handles = ts1DefService.initialize(loginHandle);
-            else
-                handles = ts1DefService.initialize(loginHandle, rics);
-
-            for (Handle nextHandle : handles) {
-                ts1DefCache.put (nextHandle, session);
-                TS1DefEntry ts1DefEntry = new TS1DefEntry ();
-                session.putTS1DefEntry(nextHandle, ts1DefEntry);
-                sessionCache.put(nextHandle, session);
-            }
-    
-            headers = message.getHeaders();
-
-            result = MessageBuilder
-                .withPayload(handles)
-                .copyHeaders(headers)
-                .build();
-        }
-
-        log.info("process: method ends; result: " +
-            ToStringBuilder.reflectionToString(result));
-
-        return result;
-    }
- */
