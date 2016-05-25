e2d7340337a40762816f1dcf0efb1ac8b2d7662c
thospfuller <thomas.fuller@coherentlogic.com>
2016-05-25 17:23:19 -0400

    This class has been deprecated.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/beans/QueryParameters.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/beans/QueryParameters.java
@@ -6,6 +6,8 @@ import com.reuters.rfa.common.Handle;
  * A bean that is passed as the message payload when querying the market price
  * service via a Spring Integration gateway.
  *
+ * @deprecated This is being replaced by domain-specific query parameters -- see {@link MarketPriceQueryParameters}.
+ *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
 public class QueryParameters extends AbstractQuery<String[]> {
