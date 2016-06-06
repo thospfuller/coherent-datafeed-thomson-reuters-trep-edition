34941f925005bc773faa73b87e1eb9b0ebd259fc
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-06 16:11:41 -0400

    Changed log levels from log.info to log.debug and replaced the Session with the SessionBean.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/adapters/FrameworkEventListenerAdapter.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/adapters/FrameworkEventListenerAdapter.java
@@ -11,8 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 
+import com.coherentlogic.coherent.datafeed.domain.SessionBean;
 import com.coherentlogic.coherent.datafeed.listeners.FrameworkEventListener;
-import com.coherentlogic.coherent.datafeed.services.Session;
 import com.reuters.rfa.common.Event;
 
 /**
@@ -42,21 +42,15 @@ import com.reuters.rfa.common.Event;
  */
 public class FrameworkEventListenerAdapter implements FrameworkEventListenerAdapterSpecification {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(FrameworkEventListenerAdapter.class);
+    private static final Logger log = LoggerFactory.getLogger(FrameworkEventListenerAdapter.class);
 
-    private final List<FrameworkEventListener>
-        initialisationSuccessfulListenerList;
+    private final List<FrameworkEventListener> initialisationSuccessfulListenerList;
 
-    private final List<FrameworkEventListener>
-        initialisationFailedListenerList;
+    private final List<FrameworkEventListener> initialisationFailedListenerList;
 
     public FrameworkEventListenerAdapter (
     ) {
-        this (
-            new ArrayList<FrameworkEventListener> (),
-            new ArrayList<FrameworkEventListener> ()
-        );
+        this (new ArrayList<FrameworkEventListener> (), new ArrayList<FrameworkEventListener> ());
     }
 
     public FrameworkEventListenerAdapter (
@@ -64,17 +58,15 @@ public class FrameworkEventListenerAdapter implements FrameworkEventListenerAdap
         List<FrameworkEventListener> initialisationFailedListenerList
     ) {
         super();
-        this.initialisationSuccessfulListenerList =
-            initialisationSuccessfulListenerList;
-        this.initialisationFailedListenerList =
-            initialisationFailedListenerList;
+        this.initialisationSuccessfulListenerList = initialisationSuccessfulListenerList;
+        this.initialisationFailedListenerList = initialisationFailedListenerList;
     }
 
     protected void notifyListeners (
-        List<FrameworkEventListener> frameworkEventListeners, Session session) {
+        List<FrameworkEventListener> frameworkEventListeners, SessionBean sessionBean) {
 
         for (FrameworkEventListener nextListener : frameworkEventListeners)
-            nextListener.onEventReceived(session);
+            nextListener.onEventReceived(sessionBean);
     }
 
 //    public void addInitialisationSuccessfulListeners (
@@ -85,10 +77,10 @@ public class FrameworkEventListenerAdapter implements FrameworkEventListenerAdap
 //    }
 
     /* (non-Javadoc)
-	 * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#addInitialisationSuccessfulListeners(java.util.List)
-	 */
+     * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#addInitialisationSuccessfulListeners(java.util.List)
+     */
     @Override
-	public void addInitialisationSuccessfulListeners (List<FrameworkEventListener> frameworkEventListeners) {
+    public void addInitialisationSuccessfulListeners (List<FrameworkEventListener> frameworkEventListeners) {
         initialisationSuccessfulListenerList.addAll(frameworkEventListeners);
     }
 
@@ -107,10 +99,10 @@ public class FrameworkEventListenerAdapter implements FrameworkEventListenerAdap
 //    }
 
     /* (non-Javadoc)
-	 * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#addInitialisationFailedListeners(java.util.List)
-	 */
+     * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#addInitialisationFailedListeners(java.util.List)
+     */
     @Override
-	public void addInitialisationFailedListeners (List<FrameworkEventListener> frameworkEventListeners) {
+    public void addInitialisationFailedListeners (List<FrameworkEventListener> frameworkEventListeners) {
         initialisationFailedListenerList.addAll(frameworkEventListeners);
     }
 
@@ -122,69 +114,69 @@ public class FrameworkEventListenerAdapter implements FrameworkEventListenerAdap
 //    }
 
     /* (non-Javadoc)
-	 * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#initialisationSuccessful(org.springframework.messaging.Message)
-	 */
+     * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#initialisationSuccessful(org.springframework.messaging.Message)
+     */
     @Override
-	public Message<?> initialisationSuccessful (Message<Event> message) {
+    public Message<?> initialisationSuccessful (Message<Event> message) {
 
         Event payload = message.getPayload();
 
-        log.info("initialisationSuccessful: method invoked; message: " +
+        log.debug("initialisationSuccessful: method invoked; message: " +
             message + ", payload: " + payload + ", handle: " + payload.getHandle());
 
         MessageHeaders headers = message.getHeaders();
 
-        Session session = (Session) headers.get(SESSION);
+        SessionBean sessionBean = (SessionBean) headers.get(SESSION);
 
-        assertNotNull ("session", session);
+        assertNotNull ("sessionBean", sessionBean);
 
-        log.info("session: " + session);
+        log.debug("sessionBean: " + sessionBean);
 
-        notifyListeners (initialisationSuccessfulListenerList, session);
+        notifyListeners (initialisationSuccessfulListenerList, sessionBean);
 
-        log.info("initialisationSuccessful: method returns.");
+        log.debug("initialisationSuccessful: method returns.");
 
         return message;
     }
 
     /* (non-Javadoc)
-	 * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#initialisationFailed(org.springframework.messaging.Message)
-	 */
+     * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#initialisationFailed(org.springframework.messaging.Message)
+     */
     @Override
-	public Message<?> initialisationFailed (Message<?> message) {
+    public Message<?> initialisationFailed (Message<?> message) {
 
         /* This should probably be a message passed in as a param since if this
          * method is called an exception will probably have been thrown.
          */
 
-        log.info("initialisationFailed: method invoked; session: " + message);
+        log.debug("initialisationFailed: method invoked; session: " + message);
 
         MessageHeaders headers = message.getHeaders();
 
-        Session session = (Session) headers.get(SESSION);
+        SessionBean sessionBean = (SessionBean) headers.get(SESSION);
 
-        assertNotNull ("session", session);
+        assertNotNull ("sessionBean", sessionBean);
 
-        notifyListeners (initialisationFailedListenerList, session);
+        notifyListeners (initialisationFailedListenerList, sessionBean);
 
-        log.info("initialisationFailed: method returns.");
+        log.debug("initialisationFailed: method returns.");
 
         return message;
     }
 
     /* (non-Javadoc)
-	 * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#getInitialisationSuccessfulListenerList()
-	 */
+     * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#getInitialisationSuccessfulListenerList()
+     */
     @Override
-	public List<FrameworkEventListener> getInitialisationSuccessfulListenerList () {
+    public List<FrameworkEventListener> getInitialisationSuccessfulListenerList () {
         return initialisationSuccessfulListenerList;
     }
 
     /* (non-Javadoc)
-	 * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#getInitialisationFailedListenerList()
-	 */
+     * @see com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapterSpecification#getInitialisationFailedListenerList()
+     */
     @Override
-	public List<FrameworkEventListener> getInitialisationFailedListenerList () {
+    public List<FrameworkEventListener> getInitialisationFailedListenerList () {
         return initialisationFailedListenerList;
     }
 }
