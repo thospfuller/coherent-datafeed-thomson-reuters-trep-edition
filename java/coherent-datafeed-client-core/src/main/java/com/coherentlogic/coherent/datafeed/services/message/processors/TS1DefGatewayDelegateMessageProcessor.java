123ba90bdfa616dc6d8461b27deae2eae6ca78cc
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-06 17:52:29 -0400

    Removed the sessionCache and added the ts1DefEntryCache. Note also that the ts1DefEntryCache and ts1DefCache caches are now instances of TS1DefEntryCache and TS1DefCache respectively. Removed the session as the SessionBean has replaced it.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TS1DefGatewayDelegateMessageProcessor.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TS1DefGatewayDelegateMessageProcessor.java
@@ -1,7 +1,8 @@
 package com.coherentlogic.coherent.datafeed.services.message.processors;
 
+import static com.coherentlogic.coherent.datafeed.misc.Constants.SESSION;
+
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
@@ -12,26 +13,28 @@ import org.springframework.messaging.MessageHeaders;
 
 import com.coherentlogic.coherent.datafeed.beans.TS1DefEntry;
 import com.coherentlogic.coherent.datafeed.beans.TS1DefQueryParameters;
+import com.coherentlogic.coherent.datafeed.caches.TS1DefEntryCache;
+import com.coherentlogic.coherent.datafeed.domain.SessionBean;
 import com.coherentlogic.coherent.datafeed.services.MessageProcessorSpecification;
 import com.coherentlogic.coherent.datafeed.services.Session;
 import com.coherentlogic.coherent.datafeed.services.TS1DefServiceSpecification;
 import com.reuters.rfa.common.Handle;
+
+import static com.coherentlogic.coherent.datafeed.misc.Utils.assertNotNull;
 
 /**
  * 
  *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class TS1DefGatewayDelegateMessageProcessor implements
-    MessageProcessorSpecification
-        <TS1DefQueryParameters, List<Handle>> {
+public class TS1DefGatewayDelegateMessageProcessor
+    implements MessageProcessorSpecification<TS1DefQueryParameters, List<Handle>> {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(TS1DefGatewayDelegateMessageProcessor.class);
+    private static final Logger log = LoggerFactory.getLogger(TS1DefGatewayDelegateMessageProcessor.class);
 
-    private final Map<Handle, Session> sessionCache;
+    private final TS1DefEntryCache ts1DefEntryCache;
 
-    private final Map<Handle, Session> ts1DefCache;
+//    private final TS1DefCache ts1DefCache;
 
     private final TS1DefServiceSpecification ts1DefService;
 
@@ -40,12 +43,12 @@ public class TS1DefGatewayDelegateMessageProcessor implements
      * @param ts1DefService
      */
     public TS1DefGatewayDelegateMessageProcessor(
-        Map<Handle, Session> sessionCache,
-        Map<Handle, Session> ts1DefCache,
+        TS1DefEntryCache ts1DefEntryCache,
+//        TS1DefCache ts1DefCache,
         TS1DefServiceSpecification ts1DefService
     ) {
-        this.sessionCache = sessionCache;
-        this.ts1DefCache = ts1DefCache;
+        this.ts1DefEntryCache = ts1DefEntryCache;
+//        this.ts1DefCache = ts1DefCache;
         this.ts1DefService = ts1DefService;
     }
 
@@ -58,50 +61,43 @@ public class TS1DefGatewayDelegateMessageProcessor implements
      * As COMPLETION_EVENTS are received these will be removed from the 
      */
     @Override
-//    @Transactional
-    public Message<List<Handle>> process(
-        Message<TS1DefQueryParameters> message) {
+    public Message<List<Handle>> process(Message<TS1DefQueryParameters> message) {
 
         TS1DefQueryParameters ts1DefQueryParameters = message.getPayload();
 
         Handle loginHandle = ts1DefQueryParameters.getLoginHandle();
 
+        SessionBean sessionBean = (SessionBean) message.getHeaders().get(SESSION);
+
+        assertNotNull("sessionBean", sessionBean);
+
         MessageHeaders headers = null;
 
         Message<List<Handle>> result = null;
 
-//        synchronized (sessionCache) {
+        String[] rics = ts1DefQueryParameters.getRics();
 
-            Session session = sessionCache.get(loginHandle);
+        log.debug("ts1DefQueryParameters: " + ts1DefQueryParameters);
 
-            String[] rics = ts1DefQueryParameters.getRics();
+        List<Handle> handles = null;
 
-            log.info("ts1DefQueryParameters: " + ts1DefQueryParameters);
+        if (rics == null || rics.length == 0)
+            handles = ts1DefService.initialize(loginHandle, sessionBean);
+        else
+            handles = ts1DefService.initialize(loginHandle, sessionBean, rics);
 
-            List<Handle> handles = null;
+        for (Handle nextHandle : handles) {
+//            ts1DefCache.put (nextHandle, session);
+            TS1DefEntry ts1DefEntry = new TS1DefEntry ();
+            ts1DefEntryCache.put(nextHandle, ts1DefEntry);
+//            sessionCache.put(nextHandle, session);
+        }
 
-            if (rics == null || rics.length == 0)
-                handles = ts1DefService.initialize(loginHandle);
-            else
-                handles = ts1DefService.initialize(loginHandle, rics);
+        headers = message.getHeaders();
 
-            for (Handle nextHandle : handles) {
-                ts1DefCache.put (nextHandle, session);
-                TS1DefEntry ts1DefEntry = new TS1DefEntry ();
-                session.putTS1DefEntry(nextHandle, ts1DefEntry);
-                sessionCache.put(nextHandle, session);
-            }
-    
-            headers = message.getHeaders();
+        result = MessageBuilder.withPayload(handles).copyHeaders(headers).build();
 
-            result = MessageBuilder
-                .withPayload(handles)
-                .copyHeaders(headers)
-                .build();
-//        }
-
-        log.info("process: method ends; result: " +
-            ToStringBuilder.reflectionToString(result));
+        log.debug("process: method ends; result: " + ToStringBuilder.reflectionToString(result));
 
         return result;
     }
@@ -125,7 +121,7 @@ public class TS1DefGatewayDelegateMessageProcessor implements
 
             String[] rics = ts1DefQueryParameters.getRics();
 
-            log.info("ts1DefQueryParameters: " + ts1DefQueryParameters);
+            log.debug("ts1DefQueryParameters: " + ts1DefQueryParameters);
 
             List<Handle> handles = null;
 
@@ -149,7 +145,7 @@ public class TS1DefGatewayDelegateMessageProcessor implements
                 .build();
         }
 
-        log.info("process: method ends; result: " +
+        log.debug("process: method ends; result: " +
             ToStringBuilder.reflectionToString(result));
 
         return result;
