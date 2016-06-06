cae86801ff4434dedfa640555e0eecb4830047cc
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-06 17:23:31 -0400

    Class reformatting and removed the sessionCache and added the timeSeriesEntriesCache. The session has been removed and a sessionBean has been added.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/QueryNextRICMessageProcessor.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/QueryNextRICMessageProcessor.java
@@ -1,18 +1,19 @@
 package com.coherentlogic.coherent.datafeed.services.message.processors;
 
-import static com.coherentlogic.coherent.datafeed.misc.SessionUtils.getSession;
+import static com.coherentlogic.coherent.datafeed.misc.Constants.SESSION;
 import static com.coherentlogic.coherent.datafeed.misc.Utils.assertNotNull;
 
-import org.infinispan.Cache;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 
 import com.coherentlogic.coherent.datafeed.beans.TimeSeriesEntries;
 import com.coherentlogic.coherent.datafeed.beans.TimeSeriesEntry;
+import com.coherentlogic.coherent.datafeed.domain.SessionBean;
 import com.coherentlogic.coherent.datafeed.domain.TimeSeriesKey;
 import com.coherentlogic.coherent.datafeed.services.MessageProcessorSpecification;
-import com.coherentlogic.coherent.datafeed.services.Session;
 import com.coherentlogic.coherent.datafeed.services.TimeSeriesServiceSpecification;
 import com.reuters.rfa.common.Handle;
 import com.reuters.rfa.session.omm.OMMItemEvent;
@@ -24,93 +25,81 @@ import com.reuters.ts1.TS1Series;
  *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class QueryNextRICMessageProcessor implements
-    MessageProcessorSpecification<OMMItemEvent, OMMItemEvent> {
+public class QueryNextRICMessageProcessor implements MessageProcessorSpecification<OMMItemEvent, OMMItemEvent> {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(QueryNextRICMessageProcessor.class);
+    private static final Logger log = LoggerFactory.getLogger(QueryNextRICMessageProcessor.class);
 
     private final TimeSeriesServiceSpecification timeSeriesService;
 
-    private final Cache<Handle, Session> sessionCache;
+    private final Map<Handle, TimeSeriesEntries> timeSeriesEntriesCache;
 
     public QueryNextRICMessageProcessor(
         TimeSeriesServiceSpecification timeSeriesService,
-        Cache<Handle, Session> sessionCache
+        Map<Handle, TimeSeriesEntries> timeSeriesEntriesCache
     ) {
         this.timeSeriesService = timeSeriesService;
-        this.sessionCache = sessionCache;
+        this.timeSeriesEntriesCache = timeSeriesEntriesCache;
     }
 
     /**
      * @todo This method is too big and should be refactored. 
      */
     @Override
-//    @Transactional
     public Message<OMMItemEvent> process(Message<OMMItemEvent> message) {
 
-        log.info("queryNextRICMessageProcessor.process: method begins; " +
-            "message: " + message);
+        log.info("queryNextRICMessageProcessor.process: method begins; message: " + message);
 
         OMMItemEvent itemEvent = message.getPayload();
 
         Handle handle = itemEvent.getHandle();
 
-        Session session = getSession(message);
+        SessionBean sessionBean = (SessionBean) message.getHeaders().get(SESSION);
 
-        TimeSeriesKey parentTimeSeriesKey = session.getTimeSeriesKey(handle);
+        TimeSeriesKey parentTimeSeriesKey = sessionBean.getTimeSeriesKey(handle);
 
-        log.info("process: method in progress; parentTimeSeriesKey: " +
-            parentTimeSeriesKey);
+        log.info("process: method in progress; parentTimeSeriesKey: " + parentTimeSeriesKey);
 
-        Handle loginHandle = session.getLoginHandle();
+        Handle loginHandle = sessionBean.getHandle();
 
-//        synchronized (sessionCache) {
+        TimeSeriesEntries timeSeriesEntries = timeSeriesEntriesCache.get(handle);
 
-            TimeSeriesEntries timeSeriesEntries =
-                session.getTimeSeriesEntries(handle);
+        String nextRic = timeSeriesEntries.nextRic();
 
-            String nextRic = timeSeriesEntries.nextRic();
+        assertNotNull("nextRic", nextRic);
 
-            assertNotNull("nextRic", nextRic);
+        log.info("Requesting the nextRic value '" + nextRic + "'.");
 
-            log.info("Requesting the nextRic value '" + nextRic + "'.");
+        String serviceName = timeSeriesEntries.getServiceName();
 
-            String serviceName = timeSeriesEntries.getServiceName();
+        int period = timeSeriesEntries.getPeriod();
 
-            int period = timeSeriesEntries.getPeriod();
+        TS1Series ts1Series = TS1Series.createSeries(nextRic, period);
 
-            TS1Series ts1Series = TS1Series.createSeries(nextRic, period);
+        Handle queryHandle = timeSeriesService.queryTimeSeriesFor(
+            serviceName, loginHandle, sessionBean, nextRic);
 
-            Handle queryHandle = timeSeriesService.queryTimeSeriesFor(
-                serviceName, loginHandle, nextRic);
+        log.info("nextRic: " + nextRic + ", queryHandle: " + queryHandle);
 
-            log.info("nextRic: " + nextRic + ", queryHandle: " + queryHandle);
+        /* We have to do this because we need to keep track of the parent
+         * RIC (ie. TRI.N) otherwise when this workflow ends, we won't know
+         * what ric the time series is associated with because every time
+         * the queryTimeSeriesFor method is called, a new handle is returned
+         * so we need to pass the parent RIC from handle to handle until
+         * there are no more RICs to query.
+         */
+        sessionBean.putTimeSeriesKey(queryHandle, parentTimeSeriesKey);
 
-            /* We have to do this because we need to keep track of the parent
-             * RIC (ie. TRI.N) otherwise when this workflow ends, we won't know
-             * what ric the time series is associated with because every time
-             * the queryTimeSeriesFor method is called, a new handle is returned
-             * so we need to pass the parent RIC from handle to handle until
-             * there are no more RICs to query.
-             */
-            session.putTimeSeriesKey(queryHandle, parentTimeSeriesKey);
+        TimeSeriesEntry timeSeriesEntry = new TimeSeriesEntry(nextRic);
 
-            TimeSeriesEntry timeSeriesEntry = new TimeSeriesEntry(nextRic);
+        timeSeriesEntry.setTs1Series(ts1Series);
 
-            timeSeriesEntry.setTs1Series(ts1Series);
+        timeSeriesEntries.putTimeSeriesEntry(queryHandle, timeSeriesEntry);
 
-            timeSeriesEntries.putTimeSeriesEntry(queryHandle, timeSeriesEntry);
+        // @todo: We're doing a lot of copying here so we may want to
+        // reconsider our approach.
+        timeSeriesEntriesCache.put(queryHandle, timeSeriesEntries);
 
-            // @todo: We're doing a lot of copying here so we may want to
-            // reconsider our approach.
-            session.putTimeSeriesEntries(queryHandle, timeSeriesEntries);
-
-            sessionCache.put(queryHandle, session);
-//        }
-
-        log.info("queryNextRICMessageProcessor.process: method ends; " +
-            "message: " + message);
+        log.info("queryNextRICMessageProcessor.process: method ends; message: " + message);
 
         return message;
     }
