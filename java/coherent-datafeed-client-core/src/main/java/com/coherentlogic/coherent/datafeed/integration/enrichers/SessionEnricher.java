168ab183e41219f27f4601e76a1991926b365ed1
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-06 16:44:38 -0400

    The addSessionToMessageHeaders now adds an instance of SessionBean to the headers (previously it was Session, which has been deprecated).

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/integration/enrichers/SessionEnricher.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/integration/enrichers/SessionEnricher.java
@@ -1,11 +1,17 @@
 package com.coherentlogic.coherent.datafeed.integration.enrichers;
 
+import static com.coherentlogic.coherent.datafeed.misc.Constants.SESSION;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 
+import com.coherentlogic.coherent.datafeed.domain.SessionBean;
+import com.coherentlogic.coherent.datafeed.exceptions.ApplicationInitializationFailedException;
+import static com.coherentlogic.coherent.datafeed.misc.Utils.assertNotNull;
 import com.coherentlogic.coherent.datafeed.services.Session;
+import com.reuters.rfa.common.Event;
 
 /**
  * A service that adds a new {@link Session} to the message under the
@@ -16,69 +22,21 @@ import com.coherentlogic.coherent.datafeed.services.Session;
  *  will not have been added to the headers -- if this has happened then this is
  *  considered to be invalid and an exception is thrown.
  *
- * @deprecated This works with only one session and we're in the process of
- *  setting this up such that we can work with any number of sessions.
- *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class SessionEnricher {
+public class SessionEnricher implements EnricherSpecification {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(SessionEnricher.class);
+    @Override
+    public Message<Event> enrich(Message<Event> message) {
 
-    private final Session session;
+        Event event = message.getPayload();
 
-    public SessionEnricher(Session session) {
-        super();
-        this.session = session;
-    }
+        SessionBean sessionBean = (SessionBean) event.getClosure();
 
-    public Message<Session> addDacsIdToSession (Message<String> message) {
-        String dacsId = message.getPayload();
+        assertNotNull ("sessionBean", sessionBean);
 
-        log.info("dacsId: " + dacsId);
-
-        session.setDacsId(dacsId);
-
-        Message<Session> result = MessageBuilder
-            .withPayload(session).build();
-
-        return result;
-    }
-
-    public Message<Session> addSessionToPayload (Message<?> message) {
-
-        log.debug ("session " + session);
-
-        Message<Session> result = MessageBuilder
-            .withPayload(session).build();
-
-        return result;
-    }
-
-    public Message<Session> addSessionToMessageHeaders (Message<?> message) {
-
-        log.debug ("session " + session);
-
-        Message<Session> result = MessageBuilder
-            .withPayload(session).build();
+        Message<Event> result = MessageBuilder.fromMessage(message).setHeaderIfAbsent(SESSION, sessionBean).build();
 
         return result;
     }
 }
-
-//.setHeader(SESSION, session).build();
-//
-//MessageHeaders headers = message.getHeaders();
-//
-//Session currentSession = (Session) headers.get(SESSION);
-//
-//if (currentSession != null)
-//  throw new ApplicationInitializationFailedException (
-//      "The session has already been added to the message headers " +
-//      "and, at this point, the expectation is that this would be " +
-//      "null.");
-//
-//Session session = new Session ();
-//
-//headers.put(SESSION, session);
