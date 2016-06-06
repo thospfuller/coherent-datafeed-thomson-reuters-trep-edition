e8dc4923ce32fc2eb6344d97425544f2597651ba
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-06 17:48:53 -0400

    Removed the session as the getTS1DefEntry is now in the ts1DefEntryCache.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TS1DefDbLoaderMessageProcessor.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/message/processors/TS1DefDbLoaderMessageProcessor.java
@@ -1,15 +1,12 @@
 package com.coherentlogic.coherent.datafeed.services.message.processors;
 
-import static com.coherentlogic.coherent.datafeed.misc.Constants.SESSION;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 
 import com.coherentlogic.coherent.datafeed.beans.TS1DefEntry;
+import com.coherentlogic.coherent.datafeed.caches.TS1DefEntryCache;
 import com.coherentlogic.coherent.datafeed.services.MessageProcessorSpecification;
-import com.coherentlogic.coherent.datafeed.services.Session;
 import com.coherentlogic.coherent.datafeed.services.TS1DefDbLoader;
 import com.reuters.rfa.common.Handle;
 import com.reuters.rfa.session.omm.OMMItemEvent;
@@ -20,31 +17,23 @@ import com.reuters.rfa.session.omm.OMMItemEvent;
  *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class TS1DefDbLoaderMessageProcessor
-    implements MessageProcessorSpecification<OMMItemEvent, OMMItemEvent> {
+public class TS1DefDbLoaderMessageProcessor implements MessageProcessorSpecification<OMMItemEvent, OMMItemEvent> {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(TS1DefDbLoaderMessageProcessor.class);
+    private static final Logger log = LoggerFactory.getLogger(TS1DefDbLoaderMessageProcessor.class);
 
     private final TS1DefDbLoader ts1DefDbLoader;
 
-    public TS1DefDbLoaderMessageProcessor(TS1DefDbLoader ts1DefDbLoader) {
-        this.ts1DefDbLoader = ts1DefDbLoader;
-    }
+    private final TS1DefEntryCache ts1DefEntryCache;
 
-    long ctr = 0;
+    public TS1DefDbLoaderMessageProcessor(TS1DefDbLoader ts1DefDbLoader, TS1DefEntryCache ts1DefEntryCache) {
+        this.ts1DefDbLoader = ts1DefDbLoader;
+        this.ts1DefEntryCache = ts1DefEntryCache;
+    }
 
     @Override
     public Message<OMMItemEvent> process(Message<OMMItemEvent> message) {
 
-        log.info("process[" + (ctr++) + "]: method begins; message: " +
-            message);
-
-        MessageHeaders headers = message.getHeaders();
-
-        Session session = (Session) headers.get(SESSION);
-
-        log.info("session: " + session);
+        log.debug("process: method begins; message: " + message);
 
         OMMItemEvent itemEvent = message.getPayload();
 
@@ -52,12 +41,11 @@ public class TS1DefDbLoaderMessageProcessor
 
         Handle handle = itemEvent.getHandle();
 
-        // TODO: test below.
-        TS1DefEntry entry = session.getTS1DefEntry(handle);
+        TS1DefEntry entry = ts1DefEntryCache.getTS1DefEntry(handle);
 
         entry.setLoaded(true);
 
-        log.info("process: method ends; message: " + message);
+        log.debug("process: method ends; message: " + message);
 
         return message;
     }
