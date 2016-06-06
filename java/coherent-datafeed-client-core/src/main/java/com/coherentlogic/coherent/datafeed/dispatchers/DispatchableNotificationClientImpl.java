b969740bf3e42186f352fcc3e225e40ee600b998
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-06 16:30:51 -0400

    Code reformatting and added the closure to a log message.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/dispatchers/DispatchableNotificationClientImpl.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/dispatchers/DispatchableNotificationClientImpl.java
@@ -3,6 +3,7 @@ package com.coherentlogic.coherent.datafeed.dispatchers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.coherentlogic.coherent.datafeed.domain.SessionBean;
 import com.coherentlogic.coherent.datafeed.exceptions.EventQueueDispatchException;
 import com.reuters.rfa.common.DeactivatedException;
 import com.reuters.rfa.common.DispatchQueueInGroupException;
@@ -15,14 +16,11 @@ import com.reuters.rfa.common.EventQueue;
  *
  * @author <a href="support@coherentlogic.com">Support</a>
  */
-public class DispatchableNotificationClientImpl implements
-    DispatchableNotificationClient {
+public class DispatchableNotificationClientImpl implements DispatchableNotificationClient {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(DispatchableNotificationClientImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(DispatchableNotificationClientImpl.class);
 
-    private static final String GENERIC_EXCEPTION_MSG
-        = "The event queue threw an exception.";
+    private static final String GENERIC_EXCEPTION_MSG = "The event queue threw an exception.";
 
     private final EventQueue eventQueue;
 
@@ -33,8 +31,7 @@ public class DispatchableNotificationClientImpl implements
      * @param eventQueue
      * @param timeout
      */
-    public DispatchableNotificationClientImpl(EventQueue eventQueue,
-        long timeout) {
+    public DispatchableNotificationClientImpl(EventQueue eventQueue, long timeout) {
         this.eventQueue = eventQueue;
         this.timeout = timeout;
     }
@@ -44,16 +41,15 @@ public class DispatchableNotificationClientImpl implements
      */
     @Override
     public void notify(Dispatchable dispatchable, Object closure) {
-        log.debug("notify: method invoked; eventQueue: " + eventQueue);
+
+        log.debug("notify: method invoked; eventQueue: " + eventQueue + ", closure: " + closure);
 
         try {
             eventQueue.dispatch(timeout);
         } catch (DeactivatedException deactivatedException) {
-            throw new EventQueueDispatchException(
-                GENERIC_EXCEPTION_MSG, deactivatedException);
+            throw new EventQueueDispatchException(GENERIC_EXCEPTION_MSG, deactivatedException);
         } catch (DispatchQueueInGroupException dispatchQueueInGroupException) {
-            throw new EventQueueDispatchException(
-                GENERIC_EXCEPTION_MSG, dispatchQueueInGroupException);
+            throw new EventQueueDispatchException(GENERIC_EXCEPTION_MSG, dispatchQueueInGroupException);
         }
     }
 }
