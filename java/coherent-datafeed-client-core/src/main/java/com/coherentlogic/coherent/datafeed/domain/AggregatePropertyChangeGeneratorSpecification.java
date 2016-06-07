dc2b794c55e368134bd43bf93b12896c9c59eb49
thospfuller <thomas.fuller@coherentlogic.com>
2016-06-07 15:06:05 -0400

    New class added.

---
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/AggregatePropertyChangeGeneratorSpecification.java
@@ -0,0 +1,26 @@
+package com.coherentlogic.coherent.datafeed.domain;
+
+import com.coherentlogic.coherent.datafeed.exceptions.FatalRuntimeException;
+
+/**
+ *
+ * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ *
+ * @param <T>
+ */
+public interface AggregatePropertyChangeGeneratorSpecification<T> {
+
+    static class MethodNotImplementedException extends FatalRuntimeException {
+
+        private static final long serialVersionUID = 5529414003900204395L;
+
+        public MethodNotImplementedException() {
+            super("This method is not yet available however if you need this functionality you can vote for it here:"
+                + "http://bit.ly/1WDA5NR");
+        }
+    }
+
+    void addAggregatePropertyChangeListener (AggregatePropertyChangeListener<T> aggregatePropertyChangeListener);
+
+    void removeAggregatePropertyChangeListener (AggregatePropertyChangeListener<T> aggregatePropertyChangeListener);
+}
