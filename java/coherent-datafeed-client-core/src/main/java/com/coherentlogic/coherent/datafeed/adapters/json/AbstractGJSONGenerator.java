ae9974b47c88c562a214cc6419ca350c42f80c3f
thospfuller <thomas.fuller@coherentlogic.com>
2016-07-29 13:27:24 -0400

    Added a new author and added documentation.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/adapters/json/AbstractGJSONGenerator.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/adapters/json/AbstractGJSONGenerator.java
@@ -4,10 +4,13 @@ import com.coherentlogic.coherent.datafeed.adapters.BasicAdapter;
 import com.google.gson.Gson;
 
 /**
- * 
- * @author thospfuller
+ * An adapter that using the <a href="https://google.github.io/gson/apidocs/">Google GSON API</a> to convert a source
+ * object of type S into a JSON String.
  *
- * @param <S>
+ * @author <a href="https://www.linkedin.com/in/thomasfuller">Thomas P. Fuller</a>
+ * @author <a href="support@coherentlogic.com">Support</a>
+ *
+ * @param <S> The source object which will be converted into a result of type String.
  */
 public abstract class AbstractGJSONGenerator<S>
     implements BasicAdapter<S, String> {
