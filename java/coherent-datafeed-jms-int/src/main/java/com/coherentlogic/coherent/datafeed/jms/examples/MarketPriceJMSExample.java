ec41dcec6a5fdd582d2f7c5ac128e2e291e8c20e
thospfuller <thomas.fuller@coherentlogic.com>
2016-08-09 16:33:25 -0400

    Reformatted code and removed unused instances.

--- java/coherent-datafeed-jms-int/src/main/java/com/coherentlogic/coherent/datafeed/jms/examples/MarketPriceJMSExample.java
+++ java/coherent-datafeed-jms-int/src/main/java/com/coherentlogic/coherent/datafeed/jms/examples/MarketPriceJMSExample.java
@@ -1,275 +1,205 @@
 package com.coherentlogic.coherent.datafeed.jms.examples;
 
-import static com.coherentlogic.coherent.datafeed.misc.Constants.AUTHENTICATION_ENTRY_POINT;
 import static com.coherentlogic.coherent.datafeed.misc.Constants.DACS_ID;
-import static com.coherentlogic.coherent.datafeed.misc.Constants.FRAMEWORK_EVENT_LISTENER_ADAPTER;
-import static com.coherentlogic.coherent.datafeed.misc.Constants.MARKET_PRICE_SERVICE_GATEWAY;
-import static com.coherentlogic.coherent.datafeed.misc.Constants.STATUS_RESPONSE_SERVICE_GATEWAY;
 
-import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.ObjectMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 
-import com.coherentlogic.coherent.datafeed.adapters.FrameworkEventListenerAdapter;
-import com.coherentlogic.coherent.datafeed.client.ui.MainUI;
+import com.coherentlogic.coherent.data.model.core.listeners.AggregatePropertyChangeEvent;
+import com.coherentlogic.coherent.data.model.core.listeners.AggregatePropertyChangeListener;
+import com.coherentlogic.coherent.datafeed.builders.ElektronQueryBuilder;
+import com.coherentlogic.coherent.datafeed.domain.MarketPrice;
 import com.coherentlogic.coherent.datafeed.domain.MarketPriceConstants;
-import com.coherentlogic.coherent.datafeed.listeners.FrameworkEventListener;
-import com.coherentlogic.coherent.datafeed.misc.Constants;
-import com.coherentlogic.coherent.datafeed.services.AuthenticationServiceSpecification;
-import com.coherentlogic.coherent.datafeed.services.MarketPriceServiceSpecification;
-import com.coherentlogic.coherent.datafeed.services.PauseResumeService;
-import com.coherentlogic.coherent.datafeed.services.Session;
-import com.coherentlogic.coherent.datafeed.services.StatusResponseServiceSpecification;
-import com.reuters.rfa.common.Handle;
+import com.coherentlogic.coherent.datafeed.domain.SessionBean;
+import com.coherentlogic.coherent.datafeed.domain.StatusResponse;
+import com.coherentlogic.coherent.datafeed.exceptions.UpdateFailedException;
+import com.coherentlogic.coherent.datafeed.services.ServiceName;
 
 /**
- * An example application that authenticates, executes a query, and gets the
- * data.
+ * An example application that authenticates, executes a query, and gets the data.
  *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 @ComponentScan(basePackages="com.coherentlogic.coherent.datafeed")
-public class MarketPriceExample implements CommandLineRunner, MarketPriceConstants {
+@ImportResource({"classpath*:spring/hornetq-beans.xml", "classpath*:spring/marketprice-jms-workflow-beans.xml"})
+public class MarketPriceJMSExample implements CommandLineRunner, MarketPriceConstants {
 
-    private static final Logger log =
-        LoggerFactory.getLogger(MarketPriceExample.class);
+    private static final Logger log = LoggerFactory.getLogger(MarketPriceJMSExample.class);
 
     @Autowired
-    private AbstractApplicationContext applicationContext;
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    @Qualifier("addMarketPriceToQueueChannel")
+    private MessageChannel addMarketPriceToQueueChannel;
+
+    private ElektronQueryBuilder elektronQueryBuilder;
 
     public static void main (String[] unused) {
 
-        SpringApplicationBuilder builder = new SpringApplicationBuilder (MarketPriceExample.class);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder (MarketPriceJMSExample.class);
 
         builder
             .web(false)
             .headless(false)
+            .registerShutdownHook(true)
             .run(unused);
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-        final MainUI mainUI = applicationContext.getBean(MainUI.class);
+        String dacsId = System.getenv(DACS_ID);
 
-        new Thread (
-            new Runnable () {
-                @Override
-                public void run() {
-                    mainUI.mainFrame.pack();
-                    mainUI.mainFrame.setVisible(true);
-                }
-            }
-        ).start();
+        elektronQueryBuilder = applicationContext.getBean(ElektronQueryBuilder.class);
 
-        final StatusResponseServiceSpecification statusResponseService =
-            (StatusResponseServiceSpecification) applicationContext.
-                getBean(STATUS_RESPONSE_SERVICE_GATEWAY);
+        SessionBean sessionBean = elektronQueryBuilder.newSessionBean(dacsId);
 
-        final AuthenticationServiceSpecification authenticationService =
-            (AuthenticationServiceSpecification) applicationContext.getBean(
-                AUTHENTICATION_ENTRY_POINT);
-
-        final FrameworkEventListenerAdapter frameworkEventListenerAdapter =
-            (FrameworkEventListenerAdapter)
-                applicationContext.getBean(FRAMEWORK_EVENT_LISTENER_ADAPTER);
-
-        final MarketPriceServiceSpecification marketPriceService =
-            (MarketPriceServiceSpecification)
-            applicationContext.getBean(MARKET_PRICE_SERVICE_GATEWAY);
-
-        final PauseResumeService pauseResumeService = new PauseResumeService ();
-
-        frameworkEventListenerAdapter.addInitialisationSuccessfulListeners (
-            new FrameworkEventListener() {
-                @Override
-                public void onEventReceived(Session session) {
-                    pauseResumeService.resume(true);
-                }
+        sessionBean.getStatusResponse().addPropertyChangeListener(
+            event -> {
+                System.out.println("statusResponse.event: " + event);
             }
         );
 
-        frameworkEventListenerAdapter.addInitialisationFailedListeners (
-            new FrameworkEventListener () {
-                @Override
-                public void onEventReceived(Session session) {
-                    pauseResumeService.resume(false);
+        elektronQueryBuilder.login(sessionBean);
+
+        AtomicLong nextUpdateCtr = new AtomicLong (0);
+
+        final MessageConsumer messageConsumer = (MessageConsumer) applicationContext.getBean("marketPriceConsumer");
+
+        new Thread (
+            () -> {
+                while (true) {
+                    MarketPrice next = getNextUpdate (messageConsumer, 5000L);
+                    System.out.println("next["+ nextUpdateCtr.incrementAndGet() +"]: " + next);
                 }
-        });
+            }
+        ).start ();
 
-        String dacsId = System.getenv(DACS_ID);
-
-        Handle loginHandle = authenticationService.login(dacsId);
-
-        log.info("main thread: " + Thread.currentThread());
-
-        boolean result = pauseResumeService.pause();
-
-        log.info("result: " + result);
-
-//        queryMarketPriceService (
-//            statusResponseService,
-//            marketPriceService,
-//            loginHandle
-//        );
+        queryMarketPriceService ();
 
         log.info("...done!");
 
-        applicationContext.close();
-
-        System.exit(-9999);
         // NOTE: If we end too soon this may be part of the reason why we're
         //       seeing an ommMsg with the final flag set -- one way of proving
         //       this would be to wait and then see if we consistently can
         //       login.
     }
 
-//    static void queryMarketPriceService (
-//        final StatusResponseServiceSpecification statusResponseService,
-//        final MarketPriceServiceSpecification marketPriceService,
-//        final Handle loginHandle
-//    ) {
-//        List<Handle> itemHandles = marketPriceService.query(
-//            Constants.dELEKTRON_DD,
-//            loginHandle,
-//            "LCOc1",
-//            "GOOG.O",
-//            "MSFT.O",
-//            "ODFL.OQ",
-//            "LKQ.OQ",
-//            "MDVN.OQ",
-//            "BFb.N",
-//            "KO.N",
-//            ".TRXFLDAFPUM11", // Equity
-//            "OIBR.K",
-//            "SWM.N",
-//            "ERICb.F",
-//            "ERICb.DE",
-//            "ERICb.D",
-//            "ERICb.BE",
-//            "ERICa.ST",
-//            "ERICa.F",
-//            "ERICa.DE",
-//            "ERICa.BE",
-//            "ERIC.W",
-//            "ERIC.PH",
-//            "ERIC.P",
-//            "ERIC.OQ",
-//            "ERIC.MW",
-//            "ERIC.DF",
-//            "ERIC.C",
-//            "ERIC.A",
-//            "DRICqf.BO",
-//            "BRICUSDNAV.DE",
-//            "BRICGBPNAV.DE",
-//            "BRICDX.MI",
-//            "BRIC.S",
-//            "BRIC.MI",
-//            "BRIC.AS",
-//            "ARICqf.BO",
-//            "ARIC.F",
-//            "ALRIC.PA1",
-//            ".VBRICUTR",
-//            "ERIC.O",
-//            "RIC.A",
-//            "ALRIC.PA",
-//            "ARIC.BO",
-//            "BRIC.L",
-//            "DRIC.BO",
-//            "AAT.N",
-//            "ABV.N",
-//            "ABVc.N",
-//            "ABX.N",
-//            "ACAS.O",
-//            "ACC.N",
-//            "ADGE.A",
-//            "AEL.N",
-//            "AEO.N",
-//            "AEP.N",
-//            "AEP_pa.N",
-//            "AFA.N",
-//            "AFE.N",
-//            "AFF.N",
-//            "AFG.N",
-//            "AFQ.N",
-//            "AFW.N",
-//            "AGM.N",
-//            "AGMa.N",
-//            "AGNC.O",
-//            "AGNCP.O",
-//            "AIG.N",
-//            "ALN.A",
-//            "AM.N",
-//            "AMID.N",
-//            "AMNB.O",
-//            "AMOV.O",
-//            "AMRB.O",
-//            "AMS.A",
-//            "AMSC.O",
-//            "AMSWA.O",
-//            "AMT.N",
-//            "AMWD.O",
-//            "AMX.N",
-//            "ANAT.O",
-//            "APEI.O",
-//            "APP.A",
-//            "AQQ.A",
-//            "ARC.N",
-//            "ARCT.O",
-//            "ARII.O",
-//            "ANAT.O",
-//            "APEI.O",
-//            "APP.A",
-//            "AQQ.A",
-//            "ARC.N",
-//            "ARCT.O",
-//            "ARII.O",
-//            "ARL.N",
-//            "ARSD.N",
-//            "ASEI.O",
-//            "ASI.N",
-//            "ASP.N",
-//            "ATAX.O",
-//            "AUQ.N",
-//            "AVD.N",
-//            "AVF.N",
-//            ".DAXBRIC",
-//            ".DAXBRICGB",
-//            ".DAXBRICGBN",
-//            ".DAXBRICGBP",
-//            ".TRXFLDAFPU", // Equity
-//            ".TRXFLDAFPUMAT" // Equity
-//        );
-//
-//        log.info ("The query is complete, now we will wait " +
-//            "for replies; itemHandles: " + itemHandles);
+    public void queryMarketPriceService () {
 
-        long ctr = 0;
+        AtomicLong statusUpdateCtr = new AtomicLong (0);
 
-//        while (true) {
-////            String nextStatusResponseUpdate = statusResponseService.getNextUpdateAsJSON("2000");
-////
-////            log.info ("nextStatusResponseUpdate[" + ctr + "]: " +
-////                nextStatusResponseUpdate);
-//
-//            String nextMarketPriceUpdate =
-//                marketPriceService.getNextUpdateAsJSON(2500L);
-//            log.info ("nextMarketPriceUpdate[" + ctr + "]: " +
-//                nextMarketPriceUpdate);
-//            System.out.println ("nextMarketPriceUpdate[" + ctr + "]: " +
-//                nextMarketPriceUpdate);
-//
-//            ctr++;
-//        }
-//    }
+        for (String nextRic : rics) {
+
+            /* We *MUST* acquire the MarketPrice instance from the Spring container because we are using AOP and if we
+             * simply create the class directly by calling the ctor, none of the property change events will fire when
+             * an update is applied and the same applies to the StatusResponse.
+             */
+            MarketPrice marketPrice = elektronQueryBuilder.newMarketPrice(nextRic);
+
+            marketPrice.addAggregatePropertyChangeListener(
+                new AggregatePropertyChangeListener<MarketPrice> () {
+
+                    @Override
+                    public void onAggregatePropertyChangeEvent(AggregatePropertyChangeEvent<MarketPrice> event) {
+
+                        MarketPrice marketPrice = event.getSource ();
+
+                        MarketPrice clone = (MarketPrice) marketPrice.clone();
+
+                        addMarketPriceToQueueChannel.send(
+                            MessageBuilder.withPayload(clone).build()
+                        );
+                    }
+                }
+            );
+
+            StatusResponse statusResponse = marketPrice.getStatusResponse();
+
+            statusResponse.addAggregatePropertyChangeListener(
+                event -> {
+
+                    System.out.println("marketPrice.statusResponse.aggregateUpdate ["+ statusUpdateCtr.incrementAndGet()
+                        + "] begins for " + marketPrice);
+
+                    event
+                        .getPropertyChangeEventMap()
+                        .forEach((key, value) -> {
+                            System.out.println("- key: " + key + ", value: " + value);
+                        }
+                    );
+
+                    System.out.println("aggregateUpdate ends.");
+                }
+            );
+
+            elektronQueryBuilder.query(ServiceName.dELEKTRON_DD, marketPrice);
+        }
+    }
+
+    public MarketPrice getNextUpdate (MessageConsumer messageConsumer, Long timeout) {
+
+        log.info("getNextUpdate: method begins; timeout: " + timeout);
+
+        Message nextMessage;
+
+        try {
+            nextMessage = messageConsumer.receive(timeout);
+        } catch (JMSException jmsException) {
+            throw new UpdateFailedException ("The next update was not received due to an exception being "
+                + "thrown while waiting to receive the next message.", jmsException);
+        }
+
+        MarketPrice result = null;
+
+        if (nextMessage != null) {
+
+            ObjectMessage nextObjectMessage = (ObjectMessage) nextMessage;
+
+            try {
+                result = (MarketPrice) nextObjectMessage.getObject();
+                nextMessage.acknowledge();
+            } catch (JMSException jmsException) {
+                throw new UpdateFailedException ("The next object could not be converted to a market price.",
+                    jmsException);
+            }
+        }
+
+        log.info("getNextUpdate: method ends; result: " + result);
+
+        return result;
+    }
+
+    public static final String[] rics = {
+        "LCOc1",
+        "GOOG.O",
+        "MSFT.O",
+        "ODFL.OQ",
+        "LKQ.OQ",
+        "MDVN.OQ",
+        "BFb.N",
+        "KO.N",
+        ".TRXFLDAFPUM11", // Equity
+        "OIBR.K",
+        "SWM.N"
+    };
 }
