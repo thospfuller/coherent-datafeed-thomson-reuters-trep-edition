a1d87b58249e926c1f956e15e520f958ed4bae7b
thospfuller <thomas.fuller@coherentlogic.com>
2016-08-16 14:51:28 -0400

    Implementation details have been added.

--- java/coherent-datafeed-rproject-int/src/main/java/com/coherentlogic/coherent/datafeed/rproject/integration/client/ElektronQueryBuilderClient.java
+++ java/coherent-datafeed-rproject-int/src/main/java/com/coherentlogic/coherent/datafeed/rproject/integration/client/ElektronQueryBuilderClient.java
@@ -2,6 +2,9 @@ package com.coherentlogic.coherent.datafeed.rproject.integration.client;
 
 import static com.coherentlogic.coherent.datafeed.misc.Constants.DACS_ID;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.jms.JMSException;
@@ -32,8 +35,16 @@ import com.coherentlogic.coherent.datafeed.domain.MarketPrice;
 import com.coherentlogic.coherent.datafeed.domain.MarketPriceConstants;
 import com.coherentlogic.coherent.datafeed.domain.SessionBean;
 import com.coherentlogic.coherent.datafeed.domain.StatusResponse;
+import com.coherentlogic.coherent.datafeed.domain.TimeSeries;
+import com.coherentlogic.coherent.datafeed.exceptions.ApplicationInitializationFailedException;
+import com.coherentlogic.coherent.datafeed.exceptions.UnknownPeriodException;
 import com.coherentlogic.coherent.datafeed.exceptions.UpdateFailedException;
 import com.coherentlogic.coherent.datafeed.services.ServiceName;
+import com.coherentlogic.rproject.integration.dataframe.adapters.RemoteAdapter;
+import com.coherentlogic.rproject.integration.dataframe.builders.JDataFrameBuilder;
+import com.coherentlogic.rproject.integration.dataframe.domain.Column;
+import com.coherentlogic.rproject.integration.dataframe.domain.JDataFrame;
+import com.reuters.ts1.TS1Constants;
 
 import net.coherentlogic.coherent.datafeed.examples.ExampleRICS;
 
@@ -57,7 +68,9 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
     private static final Logger log = LoggerFactory.getLogger(ElektronQueryBuilderClient.class);
 
     public static final String ADD_MARKET_PRICE_TO_QUEUE_CHANNEL = "addMarketPriceToQueueChannel",
-        MARKET_PRICE_CONSUMER = "marketPriceConsumer", JSON_ADAPTER = "jsonAdapter";
+        MARKET_PRICE_CONSUMER = "marketPriceConsumer", JSON_ADAPTER = "jsonAdapter",
+        TIME_SERIES_JSON_GENERATOR = "timeSeriesJSONGenerator", RFA_CLIENT = "com.reuters.rfa.common.Client",
+        DAILY = "daily", WEEKLY = "weekly", MONTHLY = "monthly";
 
     @Autowired
     private ConfigurableApplicationContext applicationContext;
@@ -67,6 +80,8 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
     @Autowired
     @Qualifier(ADD_MARKET_PRICE_TO_QUEUE_CHANNEL)
     private MessageChannel addMarketPriceToQueueChannel;
+
+    private final Map<String, Integer> periodMap;
 
     /* If you see this:
      *
@@ -81,10 +96,46 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
     @Qualifier(JSON_ADAPTER)
     private BasicAdapter<MarketPrice, String> jsonAdapter;
 
+    @Autowired
+    @Qualifier(JSON_ADAPTER)
+    private BasicAdapter<TimeSeries, String> timeSeriesJSONGenerator;
+
+    public ElektronQueryBuilderClient() {
+        this (new HashMap<String, Integer> (3));
+    }
+
+    public ElektronQueryBuilderClient(Map<String, Integer> periodMap) {
+
+        this.periodMap = periodMap;
+
+        periodMap.put(DAILY, TS1Constants.DAILY_PERIOD);
+        periodMap.put(WEEKLY, TS1Constants.WEEKLY_PERIOD);
+        periodMap.put(MONTHLY, TS1Constants.MONTHLY_PERIOD);
+    }
+
     @Override
     public void start() {
+
+        verifyRFADependencyIsPresent ();
+
         elektronQueryBuilder = applicationContext.getBean(ElektronQueryBuilder.class);
         marketPriceMessageConsumer = (MessageConsumer) applicationContext.getBean(MARKET_PRICE_CONSUMER);
+    }
+
+    void verifyRFADependencyIsPresent () {
+        try {
+            Class.forName(RFA_CLIENT);
+        } catch (ClassNotFoundException cnfe) {
+
+            String exceptionText = "Unable to instantiate the class " + RFA_CLIENT + " -- this is most likely due "
+                + "to the absence of the RFA dependency.";
+
+            RuntimeException aife = new ApplicationInitializationFailedException(exceptionText, cnfe);
+
+            log.error(exceptionText, aife);
+
+            throw aife;
+        }
     }
 
     @Override
@@ -166,7 +217,7 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
         log.info("logout: method ends.");
     }
 
-    public ElektronQueryBuilderClient query (String ric) {
+    public ElektronQueryBuilderClient queryMarketPrice (ServiceName serviceName, String ric) {
 
         log.info("query: method begins; ric: " + ric);
 
@@ -212,14 +263,26 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
             }
         );
 
-        elektronQueryBuilder.query(ServiceName.dELEKTRON_DD, marketPrice);
+        elektronQueryBuilder.query(serviceName, marketPrice);
 
         log.info("query[" + ric + "]: method ends.");
 
         return this;
     }
 
-    public MarketPrice getNextMarketPriceUpdate (Long timeout) {
+    public ElektronQueryBuilderClient queryMarketPrice (ServiceName serviceName, String... rics) {
+
+        for (String nextRic : rics)
+            queryMarketPrice (serviceName, nextRic);
+
+        return this;
+    }
+
+    public MarketPrice getNextMarketPriceUpdateAsJavaObject (String timeout) {
+        return getNextMarketPriceUpdateAsJavaObject(Long.valueOf(timeout));
+    }
+
+    public MarketPrice getNextMarketPriceUpdateAsJavaObject (Long timeout) {
 
         log.info("getNextMarketPriceUpdate: method begins; timeout: " + timeout);
 
@@ -252,15 +315,94 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
         return result;
     }
 
+    public String getNextMarketPriceUpdateAsJson (String timeout) {
+        return getNextMarketPriceUpdateAsJson(Long.valueOf(timeout));
+    }
+
     public String getNextMarketPriceUpdateAsJson (Long timeout) {
 
         log.info("getNextMarketPriceUpdateAsJson: method begins; timeout: " + timeout);
 
-        MarketPrice nextUpdate = getNextMarketPriceUpdate(timeout);
+        MarketPrice nextUpdate = getNextMarketPriceUpdateAsJavaObject(timeout);
 
         String result = jsonAdapter.adapt(nextUpdate);
 
         log.info("getNextMarketPriceUpdateAsJson: method ends; result: " + result);
+
+        return result;
+    }
+
+    protected TimeSeries getTimeSeriesFor (String serviceName, String ric, String period) {
+
+        log.debug("getTimeSeriesFor: method begins; serviceName: " + serviceName + ", ric: " + ric + ", (String) "
+            + "period: " + period);
+
+        Integer actualPeriod = periodMap.get(period);
+
+        if (actualPeriod == null)
+            throw new UnknownPeriodException ("The period '" + period + "' is invalid -- only 'daily', 'weekly', and "
+                + "'monthly' are valid values.");
+
+        TimeSeries result = elektronQueryBuilder.getTimeSeriesFor(serviceName, ric, actualPeriod);
+
+        log.debug("getTimeSeriesFor: method ends; result: " + result);
+
+        return result;
+    }
+
+    protected TimeSeries getTimeSeriesFor (String serviceName, String ric, int period) {
+
+        log.debug("getTimeSeriesFor: method begins; serviceName: " + serviceName + ", ric: " + ric + ", (int) period: "
+            + period);
+
+        TimeSeries result = elektronQueryBuilder.getTimeSeriesFor(serviceName, ric, period);
+
+        log.debug("getTimeSeriesFor: method ends; result: " + result);
+
+        return result;
+    }
+
+    public TimeSeries getTimeSeriesAsJavaObject (String serviceName, String ric, String period) {
+
+        log.info("getTimeSeriesAsJavaObject: method begins; serviceName: " + serviceName + ", ric: " + ric
+            + ", (String) period: " + period);
+
+        TimeSeries result = getTimeSeriesFor (serviceName, ric, period);
+
+        log.info("getTimeSeriesAsJavaObject: method ends; result: " + result);
+
+        return result;
+    }
+
+    // "DATE","CLOSE","OPEN","HIGH","LOW","VOLUME","ASK","BID"
+    public String getTimeSeriesAsJson (String serviceName, String ric, String period) {
+
+        log.info("getTimeSeriesAsJson: method begins; serviceName: " + serviceName + ", ric: " + ric
+            + ", (String) period: " + period);
+
+        TimeSeries timeSeries = getTimeSeriesAsJavaObject (serviceName, ric, period);
+
+        JDataFrameBuilder<String> jDataFrameBuilder = new JDataFrameBuilder<String>(
+            new JDataFrame(),
+            new RemoteAdapter()
+        );
+
+        List<String> headers = timeSeries.getHeaders();
+
+        headers.forEach(
+            header -> {
+
+                List<String> points = timeSeries.getValuesForHeader(header);
+
+                Column<String> column = new Column<String> (header, points);
+
+                jDataFrameBuilder.addColumn(column);
+            }
+        );
+
+        String result = jDataFrameBuilder.serialize();
+
+        log.info("getTimeSeriesAsJson: method ends; result: " + result);
 
         return result;
     }
@@ -278,7 +420,7 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
         new Thread (
             () -> {
                 while (true) {
-                    String next = client.getNextMarketPriceUpdateAsJson(5000L);
+                    MarketPrice next = client.getNextMarketPriceUpdateAsJavaObject(5000L);
                     System.out.println("next["+ nextUpdateCtr.incrementAndGet() +"]: " + next);
                 }
             }
@@ -287,7 +429,15 @@ public class ElektronQueryBuilderClient implements MarketPriceConstants, Startab
         client.login (dacsId);
 
         for (String nextRic : ExampleRICS.rics)
-            client.query(nextRic);
+            client.queryMarketPrice(ServiceName.ELEKTRON_DD, nextRic);
+
+//        TimeSeries timeSeries = client.getTimeSeriesAsJavaObject(ServiceName.ELEKTRON_DD.toString(), "TRI.N", "monthly");
+//
+//        System.out.println("timeSeries.samples: " + timeSeries.getSamples().size());
+//
+//        String timeSeriesJson = client.getTimeSeriesAsJson(ServiceName.ELEKTRON_DD.toString(), "TRI.N", "daily");
+//
+//        System.out.println("timeSeriesJson: " + timeSeriesJson);
 
         Thread.sleep(Long.MAX_VALUE);
 
