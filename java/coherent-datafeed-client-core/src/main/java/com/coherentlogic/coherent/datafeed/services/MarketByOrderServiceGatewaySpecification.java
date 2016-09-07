1202113c0386641c4b52c4146e659f366923e9d5
thospfuller <thomas.fuller@coherentlogic.com>
2016-09-07 12:24:35 -0400

    Fixed a typo.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketByOrderServiceGatewaySpecification.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/services/MarketByOrderServiceGatewaySpecification.java
@@ -9,7 +9,7 @@ import com.coherentlogic.coherent.datafeed.domain.SessionBean;
  * Used as a gateway interface into the Spring Integration workflow. Note that we have a very similar interface named
  * ie. MarketMakerServiceSpecification however if we use this interface the framework won't work and the reason may have
  * to do with AOP but it's not clear -- at one point we were seeing multiple method calls being executed when only one
- * was expected. Adding another interface is not ideal but the issue oes not appear if we use this interface when
+ * was expected. Adding another interface is not ideal but the issue does not appear if we use this interface when
  * calling into the integration workflow.
  *
  * @author <a href="https://www.linkedin.com/in/thomasfuller">Thomas P. Fuller</a>
