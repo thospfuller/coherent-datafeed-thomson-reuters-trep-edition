3a73feae7e007dfdeac747299c69eed50b9f6ca9
thospfuller <thomas.fuller@coherentlogic.com>
2016-09-07 12:23:33 -0400

    Added properties and associated getter and setter methods; renamed MarketPriceConstants to RDMFieldDictionaryConstants.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/MarketByPrice.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/domain/MarketByPrice.java
@@ -1,6 +1,12 @@
 package com.coherentlogic.coherent.datafeed.domain;
 
+import static com.coherentlogic.coherent.datafeed.domain.RDMFieldDictionaryConstants.ACTIV_DATE_KEY;
+import static com.coherentlogic.coherent.datafeed.domain.RDMFieldDictionaryConstants.OR_RNK_RUL;
+import static com.coherentlogic.coherent.datafeed.domain.RDMFieldDictionaryConstants.PR_RNK_RUL;
+import static com.coherentlogic.coherent.datafeed.misc.Constants.MARKET_BY_PRICE;
+
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -8,13 +14,18 @@ import java.util.List;
 import java.util.Map;
 
 import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 
 import com.coherentlogic.coherent.data.model.core.annotations.Changeable;
+import com.coherentlogic.coherent.datafeed.adapters.omm.OMMDataBufferAdapter;
 import com.coherentlogic.coherent.datafeed.adapters.omm.OMMDateTimeAdapter;
+import com.coherentlogic.coherent.datafeed.adapters.omm.OMMEnumAdapter;
 import com.coherentlogic.coherent.datafeed.adapters.omm.OMMNumericAdapter;
 import com.coherentlogic.coherent.datafeed.annotations.Adapt;
 import com.coherentlogic.coherent.datafeed.annotations.RFAType;
+import com.coherentlogic.coherent.datafeed.annotations.UsingKey;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
@@ -23,12 +34,45 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
  * @author <a href="https://www.linkedin.com/in/thomasfuller">Thomas P. Fuller</a>
  * @author <a href="support@coherentlogic.com">Support</a>
  */
+@Entity
+@Table(name=MARKET_BY_PRICE)
+@XStreamAlias(MARKET_BY_PRICE)
 public class MarketByPrice extends StatusResponseBean
     implements RICBeanSpecification, OrderEventGenerator<MarketByPrice.Order> {
+
+    private static final long serialVersionUID = -8605808741253646718L;
 
     private transient final List<OrderEventListener<MarketByPrice.Order>> orderEventListeners;
 
     private String ric;
+
+    /**
+     * WARNING: COPY/PASTED FROM MARKETPRICE SIMPLY TO TEST THIS, SO I'M NOT
+     * SURE WE NEED THIS AND HENCE I'M DEPRECATING IT.
+     *
+     * Product permissions information.
+     *
+     * PROD_PERM: UINT
+     *
+     * @deprecated See comments.
+     */
+    @XStreamAlias(RDMFieldDictionaryConstants.PROD_PERM)
+    private BigInteger permission = null;
+
+    @XStreamAlias(RDMFieldDictionaryConstants.DSPLY_NAME)
+    private String displayName;
+
+    @XStreamAlias(CURRENCY)
+    private String currency;
+
+    @XStreamAlias(ACTIV_DATE_KEY)
+    private Date activeDate;
+
+    @XStreamAlias(PR_RNK_RUL)
+    private String priceRankRule = null;
+
+    @XStreamAlias(OR_RNK_RUL)
+    private String orderRankRule = null;
 
     public MarketByPrice() {
         this (new HashMap<String, Order> (), new ArrayList<OrderEventListener<MarketByPrice.Order>> ());
@@ -39,7 +83,7 @@ public class MarketByPrice extends StatusResponseBean
         this.orderEventListeners = orderListeners;
     }
 
-    @XStreamAlias(MarketPriceConstants.ORDERS)
+    @XStreamAlias(RDMFieldDictionaryConstants.ORDERS)
     private Map<String, Order> orders;
 
     @ElementCollection
@@ -58,6 +102,71 @@ public class MarketByPrice extends StatusResponseBean
 
     public void setRic(String ric) {
         this.ric = ric;
+    }
+
+    /**
+     *
+     */
+    @RFAType(type=RDMFieldDictionaryConstants.PROD_PERM)
+    @Adapt(using=OMMNumericAdapter.class)
+    public void setPermission(@Changeable(MarketByOrder.PERMISSION) BigInteger permission) {
+        this.permission = permission;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @RFAType(type=RDMFieldDictionaryConstants.DSPLY_NAME)
+    @Adapt(using=OMMDataBufferAdapter.class)
+    public void setDisplayName(@Changeable(MarketByOrder.DISPLAY_NAME) String displayName) {
+        this.displayName = displayName;
+    }
+    
+    @UsingKey(type=CURRENCY)
+    public String getCurrency() {
+        return currency;
+    }
+
+    public static final String CURRENCY = "currency";
+
+    @RFAType(type=CURRENCY)
+    @Adapt(using=OMMEnumAdapter.class)
+    public void setCurrency(@Changeable(CURRENCY) String currency) {
+        this.currency = currency;
+    }
+
+    @UsingKey(type=ACTIV_DATE_KEY)
+    public Date getActiveDate() {
+        return activeDate;
+    }
+
+    @RFAType(type=ACTIV_DATE_KEY)
+    @Adapt(using=OMMDateTimeAdapter.class)
+    public void setActiveDate(Date activeDate) {
+        this.activeDate = activeDate;
+    }
+
+    @UsingKey(type=PR_RNK_RUL)
+    public String getPriceRankRule() {
+        return priceRankRule;
+    }
+
+    @RFAType(type=PR_RNK_RUL)
+    @Adapt(using=OMMEnumAdapter.class)
+    public void setPriceRankRule(String priceRankRule) {
+        this.priceRankRule = priceRankRule;
+    }
+
+    @UsingKey(type=OR_RNK_RUL)
+    public String getOrderRankRule() {
+        return orderRankRule;
+    }
+
+    @RFAType(type=OR_RNK_RUL)
+    @Adapt(using=OMMEnumAdapter.class)
+    public void setOrderRankRule(String orderRankRule) {
+        this.orderRankRule = orderRankRule;
     }
 
     @Transient
@@ -85,30 +194,30 @@ public class MarketByPrice extends StatusResponseBean
         );
     }
 
-    @XStreamAlias(MarketPriceConstants.ORDER)
+    @XStreamAlias(RDMFieldDictionaryConstants.ORDER)
     public static class Order extends RFABean {
 
         // REAL64
-        @XStreamAlias(MarketPriceConstants.ORDER_PRC)
+        @XStreamAlias(RDMFieldDictionaryConstants.ORDER_PRC)
         private BigDecimal orderPrice;
 
         // ENUM
-        @XStreamAlias(MarketPriceConstants.ORDER_SIDE)
+        @XStreamAlias(RDMFieldDictionaryConstants.ORDER_SIDE)
         private String orderSide;
 
-        @XStreamAlias(MarketPriceConstants.NO_ORD) //Integer / UINT64
+        @XStreamAlias(RDMFieldDictionaryConstants.NO_ORD) //Integer / UINT64
         private Integer numberOfOrders;
 
-        @XStreamAlias(MarketPriceConstants.ACC_SIZE) // Integer / Real64
+        @XStreamAlias(RDMFieldDictionaryConstants.ACC_SIZE) // Integer / Real64
         private Integer accumulatedSize;
 
-        @XStreamAlias(MarketPriceConstants.LV_TIM_MS) // Level activity time / Integer / UINT64
+        @XStreamAlias(RDMFieldDictionaryConstants.LV_TIM_MS) // Level activity time / Integer / UINT64
         private Long levelActivityTimeMillis;
 
         /**
          * The Data associated with the Level Activity Time.
          */
-        @XStreamAlias(MarketPriceConstants.LV_DATE) 
+        @XStreamAlias(RDMFieldDictionaryConstants.LV_DATE) 
         private Long levelActivityDate;
 
         public BigDecimal getOrderPrice() {
@@ -147,7 +256,7 @@ public class MarketByPrice extends StatusResponseBean
 
         public static final String ACCUMULATED_SIZE = "accumulatedSize";
 
-        @RFAType(type=MarketPriceConstants.ACC_SIZE)
+        @RFAType(type=RDMFieldDictionaryConstants.ACC_SIZE)
         @Adapt(using=OMMNumericAdapter.class)
         public void setAccumulatedSize(@Changeable(ACCUMULATED_SIZE) Integer accumulatedSize) {
             this.accumulatedSize = accumulatedSize;
@@ -159,7 +268,7 @@ public class MarketByPrice extends StatusResponseBean
 
         public static final String LEVEL_ACTIVITY_TIME_MILLIS = "levelActivityTimeMillis";
 
-        @RFAType(type=MarketPriceConstants.LV_TIM_MS)
+        @RFAType(type=RDMFieldDictionaryConstants.LV_TIM_MS)
         @Adapt(using=OMMNumericAdapter.class)
         public void setLevelActivityTimeMillis(@Changeable(LEVEL_ACTIVITY_TIME_MILLIS) Long levelActivityTimeMillis) {
             this.levelActivityTimeMillis = levelActivityTimeMillis;
@@ -171,7 +280,7 @@ public class MarketByPrice extends StatusResponseBean
 
         public static final String LEVEL_ACTIVITY_DATE = "levelActivityDate";
 
-        @RFAType(type=MarketPriceConstants.LV_DATE)
+        @RFAType(type=RDMFieldDictionaryConstants.LV_DATE)
         @Adapt(using=OMMDateTimeAdapter.class)
         public void setLevelActivityDate(@Changeable(LEVEL_ACTIVITY_DATE) Long levelActivityDate) {
             this.levelActivityDate = levelActivityDate;
