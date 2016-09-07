d33a8c08cbe547c55070d64a7891943adfede06a
thospfuller <thomas.fuller@coherentlogic.com>
2016-09-07 13:19:42 -0400

    Added an author.

--- java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/exceptions/AddFailedException.java
+++ java/coherent-datafeed-client-core/src/main/java/com/coherentlogic/coherent/datafeed/exceptions/AddFailedException.java
@@ -4,7 +4,8 @@ package com.coherentlogic.coherent.datafeed.exceptions;
  * An exception that indicates that there was a problem when executing an add
  * operation.
  *
- * @author <a href="mailto:support@coherentlogic.com">Support</a>
+ * @author <a href="https://www.linkedin.com/in/thomasfuller">Thomas P. Fuller</a>
+ * @author <a href="support@coherentlogic.com">Support</a>
  */
 public class AddFailedException extends FatalRuntimeException {
 
